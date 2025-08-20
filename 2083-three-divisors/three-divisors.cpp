8d361810ef9f8f5fffb31f99879e7c93f6deecc7
Mihirpathakji <mihirgithub123456@gmail.com>
2025-08-20 13:28:26 +0530

    Time: 2 ms (5.80%) | Memory: 7.7 MB (58.71%) - LeetSync

---
+++ 2083-three-divisors/three-divisors.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isThree(int n) {
+        int count =0;
+        for(int i =1;i*i<=n;i++){
+            if(n%i==0){
+                count++;
+                if( (i!=n/i) ){
+                    count++;
+                }
+            }
+        }
+        return count==3;
+        //Tc : sqrt(n);
+        //Sc : O(1);
+    }
+};
