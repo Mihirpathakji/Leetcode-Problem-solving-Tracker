9812c5e4c82a61247d5eaa400c2b50c7fb3b862b
Mihirpathakji <mihirgithub123456@gmail.com>
2025-10-14 02:02:13 +0530

    Time: 4 ms (1.12%) | Memory: 11.8 MB (5.68%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -2,54 +2,31 @@ class Solution {
 public:
     bool check(vector<int>& nums) {
 
-        int n=nums.size();
-        vector<int>original_nums(n);//O(n)
-        for(int i=0;i<n;i++)
-        {
-            original_nums[i]=nums[i];
-        }// O(n)
-
-        sort(original_nums.begin(),original_nums.end());// O(nlogn) original nums contains sorted elements 
-
-        // Now your nums is rotated by some number of positions .OKay i will again rotate it 
-        // by number of rotations time i.e <= nums.length()  times .And at any instant of 
-        // rotation if i  am getting the original nums back it means yes it was originally sorted 
-        //  Then lets rotate it by <= nums.length() times and lets check if it is getting equated 
-        // with the original one :
-
-        // -> For Rotating upto <= nums.length() times :Where let r be the number of rotations.
-        //Might occur that array nums given was sorted already .
-
-        
-        // This loops is to keep a count of the number of rotations we are rotating the given rotated nums
-        vector<int>rotated_nums(n);
-
-        for(int i=0;i<n;i++)
-        {
-            rotated_nums[i]=nums[i];
-        }
-        // rotated nums contains given elements 
-        //  O(n)
+// Made a sorted vector : sorted_nums
+        vector<int>sorted_nums=nums;
+        sort(nums.begin(),nums.end());
 
 
+// sorted_nums contains the sorted array.
+
+        int n =nums.size();
         for(int r=0;r<=n;r++)
-        {   
-
-            // This rotates the nums by one time : 
-            
+        {
+            bool is_sorted=true;
             for(int i=0;i<n;i++)
             {
-                rotated_nums[(i+r)%n]=nums[i];
-            } 
-            if(rotated_nums==original_nums)
+                if(nums[(i+r)%n]!=sorted_nums[i])
                 {
-                    return true;
+                    is_sorted=false;
+                    break;
                 }
+            }
+            if(is_sorted==true)
+            {
+                return true;
+            }
         }
-            return false;
-            // TC : O(n^2)
-            // Sc : O(n)
-
+        return false;
 
     }
 };
