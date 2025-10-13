278a03e657902765851e448a190c49aead99422b
Mihirpathakji <mihirgithub123456@gmail.com>
2025-10-13 22:48:59 +0530

    Time: 6 ms (0.68%) | Memory: 11.8 MB (5.67%) - LeetSync

---
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+
+        int n=nums.size();
+        vector<int>original_nums(n);//O(n)
+        for(int i=0;i<n;i++)
+        {
+            original_nums[i]=nums[i];
+        }// O(n)
+
+        sort(original_nums.begin(),original_nums.end());// O(nlogn)
+
+        // Now your nums is rotated by some number of positions .OKay i will again rotate it 
+        // by number of rotations time i.e <= nums.length()  times .And at any instant of 
+        // rotation if i  am getting the original nums back it means yes it was originally sorted 
+        //  Then lets rotate it by <= nums.length() times and lets check if it is getting equated 
+        // with the original one :
+
+        // -> For Rotating upto <= nums.length() times :Where let r be the number of rotations.
+        //Might occur that array nums given was sorted already .
+
+        
+        // This loops is to keep a count of the number of rotations we are rotating the given rotated nums
+        vector<int>rotated_nums(n);
+
+        for(int i=0;i<n;i++)
+        {
+            rotated_nums[i]=nums[i];
+        }
+
+
+        for(int r=0;r<=n;r++)
+        {   
+
+            // This rotates the nums by one time : 
+            
+            for(int i=0;i<n;i++)
+            {
+                rotated_nums[i]=nums[(i+r)%n];
+                if(rotated_nums==original_nums)
+                {
+                    return true;
+                }
+            } 
+        }
+
+            return false;
+
+
+
+    }
+};
