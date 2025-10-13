0f98f71ad12f6e9f5e65f32433bdaaa37dfbe2a7
Mihirpathakji <mihirgithub123456@gmail.com>
2025-10-14 02:38:40 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (75.56%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -2,31 +2,26 @@ class Solution {
 public:
     bool check(vector<int>& nums) {
 
-// Made a sorted vector : sorted_nums
-        vector<int>sorted_nums=nums;
-        sort(nums.begin(),nums.end());
+        //Always remmber the index which is GOING Out of the bounds say Here the index (i+1)  was going out of the bounds there by we do there (index)%n
 
+        // Most optimal solution : 
 
-// sorted_nums contains the sorted array.
+        int n=nums.size();
 
-        int n =nums.size();
-        for(int r=0;r<=n;r++)
+        int peak=0;
+
+        for(int i=0;i<n;i++)
         {
-            bool is_sorted=true;
-            for(int i=0;i<n;i++)
+            if(nums[i]>nums[(i+1)%n])
             {
-                if(nums[(i+r)%n]!=sorted_nums[i])
-                {
-                    is_sorted=false;
-                    break;
-                }
-            }
-            if(is_sorted==true)
-            {
-                return true;
+                peak++;
             }
         }
-        return false;
+
+        return peak<=1;
+
+       //Tc  : O(n)  && Sc :O(1) 
+
 
     }
 };
