300715fad1cd4bfcef5addff4048af5de864c6b5
Mihirpathakji <mihirgithub123456@gmail.com>
2025-10-18 19:35:36 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (69.84%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -2,35 +2,22 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
 
-        int n=nums.size();// 10
-         int hasharr[10001]={0};
-        
-        // Precompute : 
-        
+        int n=nums.size();
+        int total_sum=n*(n+1)/2;
+        int nums_sum=0;
         for(int i=0;i<n;i++)
         {
-            hasharr[nums[i]]+=1;//hasharr[nums[o]]+=1  -> hasharr[9]=1 i.e element 9 is present 
-            //hash[9];hash[6];hash[4];hash[2];hash[3];hash[5];hash[7];hash[0];hash[1]
-            //hash[0],hash[1],hash[2],hash[3],hash[4],hash[5],hash[6],hash[7],hash[9];but hash[8] is still ZERO SINCE ALL ARE INITIALIZED BY ZERO .
+            nums_sum+=nums[i];
         }
+        int Missing_number=total_sum-nums_sum;
+        return Missing_number;
 
-        int x=0;
-        for(int j=0;j<n;j++)
-        {
-            if(hasharr[j]==0)
-            {
-                x=j;// x=8 
-                return x;// 8 
-            }
-            //j=2 <10  j=3 <10  j=4 <10 j=5  j=6  j=7 j=8 b
-        }
-        return nums.size();
-    
         return 0;
 
 
-        //TC : O(n)
-        //Sc : O(n)
+        // TC : O(n)
+        // SC : O(1)
 
+        //Is most opitmal soln
     }
 };
