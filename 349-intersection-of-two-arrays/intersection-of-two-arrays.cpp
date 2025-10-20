455dc116deef1d6fe151c1034fae0d2874572a75
Mihirpathakji <mihirgithub123456@gmail.com>
2025-10-20 23:05:32 +0530

    Time: 2 ms (43.59%) | Memory: 14.6 MB (29.67%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -2,30 +2,31 @@ class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         
-        set<int>ans;
+        unordered_set<int>ans;
         unordered_map<int,int>mp;
         for(int i=0;i<nums1.size();i++)
         {
             mp[nums1[i]]=1;
-        }    
-
+        } 
+        //O(n)
         for(int i=0;i<nums2.size();i++)
         {
             if(mp[nums2[i]])
             {
                 ans.insert(nums2[i]);
             }
-        }
+        }//O(n)
         vector<int>nums3;
         for(auto it:ans)
         {
             nums3.push_back(it);
         }
+        //O(n)
 
         return nums3;
 
-        //TC : O(nlogn)
-        //SC : O(3*n)
+        //TC : O(3*n)==O(n)
+        //SC : O(3*n)==O(n)
 
     }
 };
