5769bd9b7808ea19b92d320baca7898f667a2c6a
Mihirpathakji <mihirgithub123456@gmail.com>
2025-11-18 01:40:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (9.11%) - LeetSync

--- 258-add-digits/add-digits.cpp
+++ 258-add-digits/add-digits.cpp
@@ -15,25 +15,16 @@ public:
         int innersum=0;
         while(sum!=0)
         {
-            int r=sum%10;//6 1 
-
-            innersum +=r;//6  +1  == 7 
-            sum/=10;//1  0 
-        }//2^n*logn 
+            int r=sum%10;//5  8  3     6    1
+            innersum +=r;//5   13  16    6   7 
+            sum/=10;//38  3  0   1  0  
+        }
         //innersum got the sum of the digits//1+1 ==2
-        return digital_root_recursion(innersum); //7
+        return digital_root_recursion(innersum); //385   16    7
     }
-
     int addDigits(int num) {
         //385
-        int sum=0;
-        while(num!=0)
-        {
-            int r=num%10;
-            sum+=r;//16
-            num/=10;
-        }
-        int x = digital_root_recursion(sum);  
+        int x = digital_root_recursion(num);  
         return x;
     }
 };
