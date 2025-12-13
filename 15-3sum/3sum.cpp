4a9a671dacc1323385766833b024bae9d1375c76
Mihirpathakji <mihirgithub123456@gmail.com>
2025-12-13 19:45:53 +0530

    Time: 47 ms (67.70%) | Memory: 28.9 MB (89.73%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,55 +1,55 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        // Two-Pointer-Technique 
-           int n=nums.size();
-           sort(nums.begin(),nums.end());
-           set<vector<int>>st;
-           int l=0;
-           int mid=l+1;
-           int r=n-1;
+        int n=nums.size();
+        int i=0;
+        int j=i+1;
+        int k=n-1;
+        sort(nums.begin(),nums.end());//nlogn
+        //[-4 -1 -1 0 1 2 ]
+        vector<vector<int>>ans;
 
-           while(l<r-1)
-           {
-                if(nums[l]+nums[mid]+nums[r]==0)
-                {
-                    st.insert({nums[l],nums[mid],nums[r]});
-                    mid++;
-                    r--;
-                    if(mid>=r)
-                    {
-                        l++;
-                        mid=l+1;
-                        r=n-1;
-                    }
+        for(int i=0;i<n-2;i++)
+        {
+            if(i>0 && nums[i]==nums[i-1]){
+                //skip the iteration for that nums[i].
+                //don't went into the while loop otherwise it will generate duplicate triplets.
+                continue;
+            }
+            j=i+1;
+            k=n-1;
+
+            while(j<k)
+            {
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum<0)
+                {   
+                    //sum<target_value
+                    //increment sum:
+                    j++;
                 }
-                else if(nums[l]+nums[mid]+nums[r]<0)
+                else if(sum>0)
                 {
-                    mid++;
-                    if(mid==r)
-                    {
-                        l++;
-                        mid=l+1;
-                        r=n-1;
-                    }
+                    //sum>target_value
+                    //decrement sum:
+                    k--;
                 }
                 else
                 {
-                    r--;
-                    if(r<=mid)
-                    {
-                        l++;
-                        mid=l+1;
-                        r=n-1;
-                    }
+                    //we get the required sum.
+                    ans.push_back({nums[i],nums[j],nums[k]});//[-1 -1  2] [-1 0 1]
+                    j++;
+                    k--;
+                    while(j<n && nums[j]==nums[j-1])j++;
+                    while(k>=0 && nums[k]==nums[k+1])k--;
                 }
-           }
-           vector<vector<int>>ans(st.begin(),st.end());
-           return ans;
+            }
+        }
+        return ans;
 
-        //TC:O(n)
-        //SC:O(n)
-
-
+        //TC:O(n^2)+O(nlogn)==O(n^2)
+        //SC:O(1)//O(no.of.triplets)->just to store the answer of input not an extra space
+    
+    
     }
 };
