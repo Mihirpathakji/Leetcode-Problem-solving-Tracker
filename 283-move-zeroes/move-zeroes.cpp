2f8b53177e098d157289dfcf04d579af42f23c98
Mihirpathakji <mihirgithub123456@gmail.com>
2025-12-15 02:15:05 +0530

    Time: 4 ms (12.01%) | Memory: 23.9 MB (19.72%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -3,30 +3,34 @@ public:
     void moveZeroes(vector<int>& nums) {
         
         int n=nums.size();
-        int j=0;
+        //count no. of zeroes in the nums:
         int count=0;
         for(int i=0;i<n;i++)
         {
-            if(nums[i]!=0)
-            {
-                nums[j]=nums[i];
-                j++;//3 
-            }
-            else
+            if(nums[i]==0)
             {
                 count++;
             }
+        }//count=2
+        int index=0;
+        for(int i=0;i<n;i++)
+        {//i==1;i==2;i==3;4;5<5 
+            if(nums[i]!=0)
+            {
+                nums[index]=nums[i];//nums[0]=nums[1]==1 nums[1]=nums[3]=3;nums[2]=nums[4]==12
+                //[1 3 12]
+                index++;//1 2 3
+            }
         }
 
-        for(int i=j;i<n;i++)
+        while(count!=0)
         {
-            nums[i]=0;
+            nums[index]=0;//   //nums[3]=0 ;nums[4]=0
+           index++; //4 5
+            count--;//1  0
         }
 
-
-        //Tc : O(n)
-        //Sc : O(1)
-
-
+        //TC:O(n)
+        //SC:O(1)
     }
 };
