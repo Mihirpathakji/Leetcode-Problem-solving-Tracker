0c071e567451827c70ceb367f085a2a952a24e7f
Mihirpathakji <mihirgithub123456@gmail.com>
2025-12-14 21:26:00 +0530

    Time: 3 ms (30.00%) | Memory: 32.2 MB (90.00%) - LeetSync

---
+++ 4158-absolute-difference-between-maximum-and-minimum-k-elements/absolute-difference-between-maximum-and-minimum-k-elements.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int absDifference(vector<int>& nums, int k) {
+        int n=nums.size();
+        if(n==1)
+        {
+            return 0;
+        }
+        sort(nums.begin(),nums.end());//[2 2 4 5]
+        int i=0;
+        int smallest_sum=0;
+        while(i<k)
+        {
+            smallest_sum+=nums[i];//100
+            i++;
+        }
+
+        int j=n-1;//0
+        int count=0;
+        int largest_sum=0;
+        while(count<k)
+        {
+            largest_sum+=nums[j];//2
+            j--;//0 -1
+            count++;//1 2 
+        }
+
+        int abs_diff=abs(smallest_sum-largest_sum);
+        return abs_diff;
+    }
+};
