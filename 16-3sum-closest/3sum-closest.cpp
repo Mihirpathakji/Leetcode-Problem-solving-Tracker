1816bf6c5648e32bc9d7f0cf9536eb04e75f3672
Mihirpathakji <mihirgithub123456@gmail.com>
2025-12-14 15:19:54 +0530

    Time: 8 ms (78.34%) | Memory: 13.9 MB (60.75%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -1,43 +1,54 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
         int n=nums.size();
-        int i=0;    
-        int sum;
-        vector<int>ans;
-        unordered_map<int,int>mp;
-        for(int i=0;i<n-2;i++)
-        {    sum=0;
-             int j=i+1;//2
-             int k=n-1;//n-1
-            while(j<k)
+        sort(nums.begin(),nums.end());//O(nlogn)
+        int mini_absdiff=INT_MAX;
+        int closest_sum=0;//That what we wanted to find out.
+
+    //  Technique to Generate all the Triplets:
+        for(int k=0;k<n-2;k++)
+        {   
+            int i=k+1;
+            int j=n-1;
+            //Keeping k constant and only changing the i and j two pointers.
+            while(i<j)
             {
-                sum=nums[i]+nums[j]+nums[k];
+                int sum=nums[k]+nums[i]+nums[j];
                 if(sum==target)
                 {
                     return sum;
                 }
                 else if(sum<target)
                 {
-                    j++;//2
-                    int abs_diff=abs(sum-target);
-                    ans.push_back(abs_diff);
-                    mp[abs_diff]=sum;
+                    //-3<1  ->  
+                    int curr_absdiff=abs(sum-target);
+                    if(curr_absdiff<mini_absdiff)
+                    {
+                        //sum is closer 
+                        mini_absdiff=curr_absdiff;
+                        closest_sum=sum;
+                    }
+                    i++;
                 }
                 else
                 {
-                    k--;
-                    int abs_diff=abs(sum-target);
-                    ans.push_back(abs_diff);
-                    mp[abs_diff]=sum;
+                    //i would calculate the minimum absolute difference.
+                    int curr_absdiff=abs(target-sum);
+                    if(curr_absdiff<mini_absdiff)
+                    {   
+                        mini_absdiff=curr_absdiff;
+                        closest_sum=sum;
+                    }
+                    j--;
                 }
             }
         }
-        int mini=*min_element(ans.begin(),ans.end());
-        return mp[mini];
+        return closest_sum;
+
+        //TC :O(n^2+nlogn)
+        //SC:O(1)
+
+
     }
-    
-    //TC:O(n^2+nlogn)
-    //SC:O(n.of.triplets.formed)
 };
