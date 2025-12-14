cb233cc1b14201fa639684cd4dff769d45c1a5d3
Mihirpathakji <mihirgithub123456@gmail.com>
2025-12-14 18:01:34 +0530

    Time: 27 ms (24.59%) | Memory: 17.4 MB (82.57%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,52 +1,58 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-    sort(nums.begin(),nums.end());
-    int n=nums.size();
-    int a=0;
-    int b=a+1;
-    vector<vector<int>>ans;
+        int n=nums.size();
+        int a=0;
+        int b=a+1;
+        vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());//Without sorting the Two-Pointer Technique cant be used.
 
-    //[-2 -1 -1 1 1 2 2 ]n=7
-    //Generating Qudrupulet of 4-Numbers: 
-    for(int a=0;a<n-3;a++)
-    {
-        //a==1
-        if(a>0 && nums[a]==nums[a-1])continue;
-          for(int b=a+1;b<n-2;b++)
-           {//b=2 3 
-                if(b>1 && nums[b]==nums[b-1] && b!=a+1)continue;
+        //Two-Pointer-Technique.Move only the Two-Pointers c and d Only Move the two -pointers other all pointers must be kept constant. 
 
-                int c=b+1;//3
-                int d=n-1;//6
+        //Generate-Quadrupulets.
+
+        for(int a=0;a<n-3;a++)
+        {
+            if(a>0 && nums[a]==nums[a-1])continue;
+            for(int b=a+1;b<n-2;b++)
+            {
+                if(b>1 && nums[b]==nums[b-1] && b!=a+1)continue;  
+
+                int c=b+1;
+                int d=n-1;
+                //Move only the Two-Pointers c and d Kept all the other Pointers a,b constant 
                 while(c<d)
                 {
-                    long long int sum=((long long int)nums[a]+(long long int )nums[b]+(long long int)nums[c]+(long long int)nums[d]);//-2//2 //2 
+                    long long int sum=(long long int)nums[a]+(long long int)nums[b]+(long long int)nums[c]+(long long int)nums[d];
                     if(sum==target)
                     {
-                        ans.push_back({nums[a],nums[b],nums[c],nums[d]});//[-2 -1 1 2 ],[-1 -1 1 1]
-                        c++;//4 5 
-                        d--;//5 3
-                        while(c<d && nums[c]==nums[c-1]) {
-                            c++;//5
+                        ans.push_back({nums[a],nums[b],nums[c],nums[d]});
+                        c++;
+                        while(c<d && nums[c]==nums[c-1])
+                        {
+                            c++;
                         }
-                        while(c<d &&  nums[d]==nums[d+1])  {
+                        d--;
+                        while(d>=0 && nums[d]==nums[d+1])
+                        {
                             d--;
-                        }           
+                        }
                     }
                     else if(sum<target)
                     {
-                        c++;//3
+                        c++;
                     }
                     else
                     {
-                        d--;//5  4 
+                        d--;
                     }
-                }   
-             }
-    }
-    return ans;
-    //TC:O(n^3+nlogn)
-    //SC:O(no.of.unique.Quadrupulets)
+                }
+            }
+        }
+        return ans;
+
+        //TC:O(n^3+nlogn)
+        //SC:O(1)(NO EXTRA SPACE)
+
     }
 };
