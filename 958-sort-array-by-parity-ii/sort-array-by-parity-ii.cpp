c625990c4498b2325bf6674acd69ef17bf2025d8
Mihirpathakji <mihirgithub123456@gmail.com>
2025-12-15 13:45:54 +0530

    Time: 0 ms (100.00%) | Memory: 26 MB (21.83%) - LeetSync

--- 958-sort-array-by-parity-ii/sort-array-by-parity-ii.cpp
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.cpp
@@ -2,35 +2,25 @@ class Solution {
 public:
     vector<int> sortArrayByParityII(vector<int>& nums) {
         int n=nums.size();
-        int i=0;
-        int j=n-1;
-        vector<int>ans(n);
-        int evenindex=0;
-        int oddindex=1;
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]%2==0)
+        int i=0;//even index 
+        int j=1;//odd index
+        while(i<n && j<n)
+        {   
+            while(i<n && nums[i]%2==0)    
             {
-                ans[evenindex]=nums[i];//ans[0]=nums[0]==4  ans[2]=nums[1]==2
-                evenindex+=2;//2
-                //ans[]=[4 5  2 7]
-                //ans[0]=2
-            }   
-            else
-            {
-                ans[oddindex]=nums[i];//ans[1]=nums[2]==5;ans[3]=nums[3]==7
-                oddindex+=2;//3;
-                //ans[1]=nums[1]==3
+                i+=2;
             }
-            //i==1;2;3
-            //i==1;2
+            
+            while(j<n && nums[j]%2==1)    
+            {
+                j+=2;
+            }
+
+            if(i<n && j<n)
+            {
+                swap(nums[i],nums[j]);
+            }  
         }
-        return ans;
-
-    //[2,3]->   n=2
-    //ans[]=[2 3]
-        //TC:O(n)
-        //SC:O(n)
-
+        return nums;
     }
 };
