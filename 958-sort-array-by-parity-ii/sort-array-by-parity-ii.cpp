205883c196a58eca220d833195e51a6188c22e40
Mihirpathakji <mihirgithub123456@gmail.com>
2025-12-15 16:41:28 +0530

    Time: 2 ms (27.32%) | Memory: 26 MB (47.57%) - LeetSync

--- 958-sort-array-by-parity-ii/sort-array-by-parity-ii.cpp
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.cpp
@@ -3,24 +3,31 @@ public:
     vector<int> sortArrayByParityII(vector<int>& nums) {
         int n=nums.size();
         int i=0;//even index 
-        int j=1;//odd index
+        int j=1;//odd index 
         while(i<n && j<n)
-        {   
-            while(i<n && nums[i]%2==0)    
+        {
+            if(nums[i]%2==0)
             {
+                //even index contains an even element which is Good for sure.Lets check for the next i value.
                 i+=2;
             }
-            
-            while(j<n && nums[j]%2==1)    
+            else if(nums[j]%2==1)
             {
+                //odd index contains an odd element which is Good for sure,lets check for the next j value.
                 j+=2;
             }
-
-            if(i<n && j<n)
+            else
             {
+                //nums[i] which should be even is an Odd number and the nums[j] which should be odd is an even number .Both of them had the opposite Parity of what is wanted actually in both 
                 swap(nums[i],nums[j]);
-            }  
+                i+=2;
+                j+=2;
+            }
         }
         return nums;
+
+        //TC:O(n)
+        //SC:O(1)
+
     }
 };
