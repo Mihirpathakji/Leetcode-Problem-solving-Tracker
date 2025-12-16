f80e6770452e97f6bd064dba3a7145a6729d11bf
Mihirpathakji <mihirgithub123456@gmail.com>
2025-12-16 23:35:36 +0530

    Time: 1 ms (91.75%) | Memory: 128.2 MB (78.31%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-      int n=nums.size();
-      vector<int>positives;
-      vector<int>negatives;
-
+        int n=nums.size();
+        int evenindex=0;//would track all the evenindexes
+        int oddindex=1;//would track all the oddindexes
+        vector<int>ans(n);
         for(int i=0;i<n;i++)
         {
-            if(nums[i]<0)
+            if(nums[i]>0)
             {
-                negatives.push_back(nums[i]);
+                ans[evenindex]=nums[i];//ans[0]=nums[0]=3   ans[2]=nums[1]=1  //ans[4]=nums[4]==2
+                evenindex+=2;//2 4 6
             }
             else
             {
-                positives.push_back(nums[i]);
+                ans[oddindex]=nums[i];//ans[1]=nums[2]=-2   //ans[3]=nums[3]=-5//ans[5]=nums[5]=-4
+                oddindex+=2;//3//5
             }
+            //i==1;2;3;4;5
+            //ans[]=[3 -2  1 -5 2 -4]
         }
-        int j=0;
-        int i=0;
-        for(int index=0;index<n;index++)
-        {//index==1;2;3;4;5
-            if(index%2==0)
-            {
-                nums[index]=positives[i];//nums[0]=positives[0]==3
-                i++;//1 nums[2]=positives[1]==1//2;nums[4]=positives[2]==2
-            }   
-            else
-            {
-                nums[index]=negatives[j];//nums[1]=negatives[0]=-2//nums[3]=-5
-                j++;//1//2 nums[5]==-4
-            }
-        }
-    //nums[]=[3 -2 1 -5 2 -4]
-        return nums;
+            return ans;
 
-        //TC:O(n)
-        //SC:O(n)
+            //TC:O(n)
+            //SC:O(n)
     }
 };
