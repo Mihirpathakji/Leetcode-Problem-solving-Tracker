f50395fc7165bdd68c329eab2daafd4d0365a4cd
Mihirpathakji <mihirgithub123456@gmail.com>
2025-12-22 11:12:24 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (62.34%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,30 +1,51 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        //Optimal Solution:
+        //Moree's Voting Algorithm:TC:O(n) & SC:O(1)
         int n=nums.size();
-        int el;
+        int element=-1;
         int count=0;
         for(int i=0;i<n;i++)
         {
             if(count==0)
             {
-                el=nums[i];
-                count++;
+                count=1;
+                element=nums[i];
             }
-            else if(el==nums[i])
+            else if(nums[i]==element)
             {
-                count++;
+                count++;//Tracks the frequency of that element.
             }
             else
             {
                 count--;
             }
         }
-    
-        return el;
+        //element would have got the Majority element in it since it is mentioned in Question that the Majority element always exists.
+        
+        return element;
+
+        //suppose it dosent exists than we need to do a check and return -1  since the majority element dosent exists.You need to track the frequency of the element that you have founded check whether it is greater than n/2 or not??
+
+        int freq=0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==element)
+            {
+                freq++;
+            }
+        }
+        if(freq<=n/2)
+        {
+            return -1;
+        }
+        else
+        {
+            return element;
+        }
+
+
         //TC:O(n)
         //SC:O(1)
-    
     }
 };
