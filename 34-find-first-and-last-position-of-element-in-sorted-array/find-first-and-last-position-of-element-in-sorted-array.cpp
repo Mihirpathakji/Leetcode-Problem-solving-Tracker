80a58603a6119b59d00a150c2ec2604534869ef1
Mihirpathakji <mihirgithub123456@gmail.com>
2025-12-25 00:25:07 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (51.37%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,23 +1,64 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        //O(n) Good solution:
         int n=nums.size();
-        int first=-1;
-        int last=-1;
-        for(int i=0;i<n;i++)
+        //First Position of the element can be founded by Doing one Binary Search:
+        int low=0;
+        int high=n-1;//0-1==-1
+        int first_occurance=-1;
+        int mid;
+        //1.Binary_Search To find the First Occurance (index) of target.
+        while(low<=high)
         {
-            if(nums[i]==target)
+            mid=low+(high-low)/2;
+            if(nums[mid]==target)
             {
-                if(first==-1)
-                {
-                    first=i;   
-                }
-                last=i;
+                //target is present store that index in variable look for the First Occurance of that target.
+                first_occurance=mid;
+                //Wanted the First Occurance of mid->Move towards the Left side:
+                high=mid-1;
+            }
+            else if(nums[mid]>target)
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;    
             }
         }
-        return {first,last};
+        if(first_occurance ==-1)
+        {
+            return {-1,-1};            
+        }
+
+        //2.Again binary search to find the Last Occurance ( index ) of Target .
+
+        low=0;
+        high=n-1;
+        int last_occurance=-1;
+        while(low<=high)
+        {
+            mid=low+(high-low)/2;
+            if(nums[mid]==target)
+            {
+                last_occurance=mid;
+                //Look for the Last Occurance of that element.
+                //Move towards Right.
+                low=mid+1;
+            }
+            else if(nums[mid]>target)
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+
+        return {first_occurance,last_occurance};
+        //TC:O(log2(n))
+        //SC:O(1)
     }
-    //TC:O(n)
-    //SC:O(1)
 };
