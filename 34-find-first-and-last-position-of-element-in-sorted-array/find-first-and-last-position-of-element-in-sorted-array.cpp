5828b8083be40991f99653cd04fe0f0216a34129
Mihirpathakji <mihirgithub123456@gmail.com>
2025-12-24 19:23:36 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (16.53%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        if(binary_search(nums.begin(),nums.end(),target)){
-            // First Occurence =lower_bound()
-            int result=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
-            // Last Occurence = upper_bound()-1
-            int result1=upper_bound(nums.begin(),nums.end(),target)-nums.begin()-1; 
-            return {result,result1};
-        }
+        int lb=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+        int up=upper_bound(nums.begin(),nums.end(),target)-nums.begin();
+        if(lb==up)
+        {
             return {-1,-1};
+        }
+        return {lb,up-1};
+        //TC:O(log2(n))
+        //SC:O(1)
     }
-
-    
-    // Tc:O(3logn)
-    // Sc:O(1)
-
-
 };
