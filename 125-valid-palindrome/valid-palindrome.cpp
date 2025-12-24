93a9c50667a412e812a97fcf6f8a179aa5631d4d
Mihirpathakji <mihirgithub123456@gmail.com>
2025-12-24 11:00:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (78.11%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,41 +1,43 @@
 class Solution {
 public:
-    bool isPalindrome(string s) {
-        string s1="";
-        int n=s.size();
-        for(int i=0;i<n;i++)
+    bool isalphanumeric(char ch)
+    {
+        if((ch>='a' && ch<='z')  ||  (ch>='A' && ch<='Z')|| (ch>='0' && ch<='9'))
         {
-            if((s[i]>=65 && s[i]<=90))
-            {
-                s[i]+=32;
-            }
+            return true;
         }
-        //a man,a plan, a canal: panama
-        for(int i=0;i<n;i++)
-        {//taking only of if there is alpha numeric:
-            if((s[i]>=65 && s[i]<=90)|| (s[i]>=97 && s[i]<=122) || (s[i]=='0' ||s[i]=='1'||s[i]=='2'||s[i]=='3'||s[i]=='4'||s[i]=='5'||s[i]=='6'||s[i]=='7'||s[i]=='8'||s[i]=='9'))
-            {
-                s1.push_back(s[i]);
-            }
-        }
-        int n1=s1.size();
-        //check if s1 is palindromic or not:
+        return false;
+    }
+
+
+    bool isPalindrome(string s) {
+        int n=s.size();
+        //Two - pointer Approach:
         int start=0;
-        int end=n1-1;
+        int end=n-1;
         while(start<end)
         {
-            if(s1[start]!=s1[end])
+            if(!isalphanumeric(s[start]))
+            {
+                start++;//space. 
+            }
+            if(!isalphanumeric(s[end]))
+            {
+                end--;
+            }
+            if((isalphanumeric(s[start])  && isalphanumeric(s[end]) ) && ( tolower(s[start])!=tolower(s[end]) ) )
             {
                 return false;
             }
-            else
+            else if((isalphanumeric(s[start])  && isalphanumeric(s[end]) ) && ( tolower(s[start])==tolower(s[end]) ))
             {
                 start++;
                 end--;
             }
-        }
+        } 
         return true;
+
         //TC:O(n)
-        //SC:O(n)
+        //SC:O(1)
     }
 };
