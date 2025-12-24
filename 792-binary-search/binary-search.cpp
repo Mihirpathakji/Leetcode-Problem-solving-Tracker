d593303345a84acac001f9ddcafb092d7657dd93
Mihirpathakji <mihirgithub123456@gmail.com>
2025-12-24 15:38:58 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (79.44%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
+
+    int binary_search(vector<int>&arr,int low,int high,int target)
+    {
+
+        //base case:
+        if(low>high)
+        {
+            return -1;
+        }
+        int mid=low+ (high-low)/2;
+        if(arr[mid]==target)
+        {
+            return mid;
+        }
+        else if(arr[mid]>target)
+        {
+            return binary_search(arr,low,mid-1,target);
+        }
+        else
+        {
+            return binary_search(arr,mid+1,high,target);
+        }
+    }
     int search(vector<int>& nums, int target) {
         int n=nums.size();
-        int start=0;
-        int end=n-1;
-        while(start<=end)
-        {
-            int mid=end-(end-start)/2;
-            if(nums[mid]==target)
-            {
-                return mid;
-            }
-            else if(nums[mid]>target)
-            {
-                end=mid-1;
-            }
-            else
-            {
-                start=mid+1;
-            }
-        }
-        return -1;
-
-        //TC:O(log2(n))
-        //SC:O(1)
+        return binary_search(nums,0,n-1,target);
     }
+
+
+    //TC:O(log2(n))
+    //SC:O(log2(n))//The Stack Space that you are using in the Recursion to hold all recursive calls.
 };
