4d3c4b23739cce5ca8fc23b5b6ebc97ce17dcd06
Mihirpathakji <mihirgithub123456@gmail.com>
2025-12-25 14:56:12 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (65.30%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -2,64 +2,55 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int n=nums.size();
-        int index=n;
-        bool flag=false;
-        bool slag=false;
-        for(int i=0;i<n-1;i++)
-        {
-            if(nums[i+1]<nums[i])
-            {
-                slag=true;
-                index=i+1;//4
-                break;
-            }
-        }
-
-        //1.Binary search on left part:
         int low=0;
-        int high=index-1;//3
+        int high=n-1;
         int mid;
         while(low<=high)
         {
             mid=low+(high-low)/2;
-            if(nums[mid]==target)
-            {
-                flag=true;
-                return mid;
-            }
-            else if(nums[mid]>target)
-            {
-                high=mid-1;
-            }
-            else
-            {
-                low=mid+1;
-            }
-        }
-        if(flag==false && slag==false)
-        return -1;
 
-        low=index;//i+1
-        high=n-1;//0
-        while(low<=high)
-        {
-            mid=low+(high-low)/2;//0//-1
             if(nums[mid]==target)
             {
                 return mid;
             }
-            else if(nums[mid]>target)
+            //1.If nums[mid]!=target->This is an Rotated sorted Array You need to check Which portion is sorted.Whether the Left portion is sorted or right?
+            //Is Left Portion sorted??
+            else if(nums[low]<=nums[mid])
             {
-                high=mid-1;//0-1==-1
+                //Yes.
+                //Is my element lying on left sorted part?
+                if(nums[low]<=target && target<nums[mid])
+                {
+                    //Yes.
+                    //search only on that "left" part.
+                    high=mid-1;
+                }
+                else
+                {
+                    //Left portion sorted but element not on left part->Search on right part:
+                    low=mid+1;
+                }
             }
             else
             {
-                low=mid+1;
+                //Left portion is "not" sorted ->Right Portion "must be" sorted
+                //Okay then check if my target lying on right part or not!
+                if(nums[mid]<target && target<=nums[high])
+                {
+                    //Yes.
+                    //Search only on right part ."Eliminate the Left search space"
+                    low=mid+1;
+                }
+                else
+                {
+                    //right part sorted but target not on right part .
+                    //i.e search on the left part only .Eliminate the right search space.
+                    high=mid-1;
+                }
             }
         }
-
-        return -1;
-        //TC:O(log2(n))
-        //SC:O(1)
+            return -1;
+            //TC:O(log2(n))
+            //SC:O(1)
     }
 };
