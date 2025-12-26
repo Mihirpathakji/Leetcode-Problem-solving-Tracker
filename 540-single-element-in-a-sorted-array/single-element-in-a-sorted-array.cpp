2dde28cd2016cc25d0d68f4bc1b9540b3e0b59b6
Mihirpathakji <mihirgithub123456@gmail.com>
2025-12-26 21:37:35 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (67.25%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,21 +1,44 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        //Brute Force :
-        int n=nums.size();//[1]
-        for(int i=1;i<n;i++)
-        {//i==2
-            if(nums[n-1]!=nums[n-2])
-            {
-                return nums[n-1];
-            }
-            if(nums[i]!=nums[i+1]  && nums[i]!=nums[i-1])
-            {
-                return nums[i];
-            }
+        int n=nums.size();
+        if(n==1)
+        {
+            return nums[0];
         }
-        return nums[0];
-        //TC:O(n)
+        if(nums[0]!=nums[1])
+        {
+            return nums[0];
+        }
+        if(nums[n-1]!=nums[n-2])
+        {
+            return nums[n-1];
+        }
+        //Apply binary search on the Remaining search space:
+        //1.Try to eliminate the 
+        int low=0;
+        int high=n-1;
+        int mid;
+        while(low<=high)
+        {
+            mid=low+(high-low)/2;
+            //Check if this an single element:
+            if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1])
+            {
+                return nums[mid];
+            }
+            //condition is if() Just observe it and code it
+            if(mid%2==0 && nums[mid]==nums[mid-1]  || mid%2==1 && nums[mid]==nums[mid+1])
+            {
+                high=mid-1;//3
+            }
+            else
+            {
+                low=mid+1;
+            }    
+        }
+        return -1;
+        //TC:O(log2(n))
         //SC:O(1)
     }
 };
