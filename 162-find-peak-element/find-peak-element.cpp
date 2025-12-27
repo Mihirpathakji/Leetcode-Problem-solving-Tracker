0b8048f19a586fadc88ad4ae1f94d3107b45142e
Mihirpathakji <mihirgithub123456@gmail.com>
2025-12-27 06:30:33 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (49.92%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+            int maxi=INT_MIN;
+            int index=-1;
+            for(int i=0;i<nums.size();i++)
+            {
+                if(nums[i]>=maxi)
+                {
+                    maxi=nums[i];
+                    index=i;//0
+                }
+            }
+            return index;
+        }
+};
