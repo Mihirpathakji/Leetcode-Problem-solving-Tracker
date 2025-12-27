63534455588130ba11a594da9a4e26c4020a3ef9
Mihirpathakji <mihirgithub123456@gmail.com>
2025-12-27 06:40:44 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (49.92%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-            int maxi=INT_MIN;
-            int index=-1;
-            for(int i=0;i<nums.size();i++)
+            int n=nums.size();
+            if(n==1)
             {
-                if(nums[i]>=maxi)
+                return 0;
+            }
+            if(nums[0]>nums[1])
+            {
+                return 0;
+            }
+            if(nums[n-1]>nums[n-2])
+            {
+                return n-1;
+            }   
+            for(int i=1;i<nums.size()-1;i++)
+            {//nums[i] is greater than both of its's adjacent element(neighbors) than it is the peak element.
+                if(nums[i]>nums[i+1] && nums[i]>nums[i-1])
                 {
-                    maxi=nums[i];
-                    index=i;//0
+                    return i;
                 }
             }
-            return index;
+            //TC:O(n)
+            //SC:O(1)
+            return 0;
         }
 };
