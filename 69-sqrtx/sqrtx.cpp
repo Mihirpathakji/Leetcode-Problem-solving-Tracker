f9290ef6381c0b067e203a32d68f73c0009d6291
Mihirpathakji <mihirgithub123456@gmail.com>
2025-12-27 10:46:20 +0530

    Time: 22 ms (5.21%) | Memory: 8.5 MB (49.71%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
     int mySqrt(int x) {
-// the sqrt(x) always lies between 0  and the number  x . We can consider the 0 as the start and the x as the end therby apply the simple binar search
-
-    int start = 0;
-    int end = x;
-   long long int mid ;
-    long long int ans = -1;
-
-    while(start <= end){
-            mid  = start  + (end - start)/2;
-
-            if(mid*mid == x){
-                //i.e the mid is the sqrt of the x 
-                return mid;  // return that sqrt (x)
+        //Brute Force :looping from 1 till root x
+        //x=256;
+        if(x==0)
+        {
+            return 0;
+        }
+        int ans=1;//1;
+        for(long long int i=1;i*i<=x;i++)
+        {//i*i=1<=256  ;i*i=4<=256;i=3;i=4;i=5;i=6;i=7;i=8;i=9;i=10;i=11;i=12;i=13;i=14;i=15';i=16;i=17
+            if(i*i<=x)
+            {//1<=1
+                ans=i;//1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
             }
-            else if(mid * mid < x ){
-                // lekin for the x = 10 mid *mid is 4 then 4 is the possible answer of sqrt(10) so you need to store it in another variable 
-                ans = mid;
-                start = mid + 1;
-            }
-            else{
-                end = mid -1;
-            }
-    }
+        }
         return ans;
+        //TC:O(Sqrt(n))
+        //SC:O(1)
     }
 };
