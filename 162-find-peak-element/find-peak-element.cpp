8a25315db1ed731cc7c1177293fa90cda211122f
Mihirpathakji <mihirgithub123456@gmail.com>
2025-12-27 09:22:48 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (79.83%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -15,8 +15,13 @@ public:
             return n-1;
         }
         //I had already checked for i==0 and i==n-1 .Now If the answer were at those indices then it could have been returned.Now ,If it not at those indices than why to include those indices in my search space when i already know muy answer is not the index=0 or index =n-1;Then i would not include them in my search space.  
+
+        // We had already covered if i==0 i==n-1 gives answer.Now if it dosent gives us the answer better is EXCLUDE them from our search space .Hence took low=1 AND high=n-2 mid will never be 0 and never be n-1 by this search space.
         int low=1;//0
         int high=n-2;//4
+
+        //Appraoch:nums[mid] can have only 4 possibilities with respect to peak element.Our target is to get that peak element. 
+
 
         while(low<=high)
         {//0<=4 //0<=1
