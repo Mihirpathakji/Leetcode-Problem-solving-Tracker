508233bbef910f86bf452222156c219b646993cd
Mihirpathakji <mihirgithub123456@gmail.com>
2026-01-17 15:20:49 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (9.06%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,50 +1,45 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-
-        //Brute force:
+        
         vector<int>nums3(m+n);
-        int i     = 0;
-        int j     = 0;
+        int i = 0;
+        int j = 0;
         int index = 0;
 
-        if(n == 0)
-        {
-            return;
-        }
-
-        while(i < m  && j<n)
+        while( i < m && j < n)
         {
             if(nums1[i] <= nums2[j])
             {
                 nums3[index] = nums1[i];
-                index++;//1 2 4
-                i++;//1 2 3
+                i++;//2
+                index++;//2
             }
             else
             {
-                nums3[index] = nums2[j];   
-                j++;//1
-                index++;//1
+                nums3[index] = nums2[j];
+                j++;
+                index++;
             }
         }
 
-        if(j == n)
+        if(i == m)
+        {
+            //no elements remained from nums1[]  there may be elements from nums2[] yet to be filled.
+            while(j<n)
+            {
+                nums3[index] = nums2[j];
+                j++;
+                index++;
+            }
+        }
+        
+        else
         {
             while(i < m)
             {
                 nums3[index] = nums1[i];
                 i++;
-                index++;
-            }
-        }
-        else
-        {
-            //i == m
-            while(j < n)
-            {
-                nums3[index] = nums2[j];
-                j++;
                 index++;
             }
         }
