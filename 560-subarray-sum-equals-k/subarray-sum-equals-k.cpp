11403b4f62058df61d69a1b888d707b9bdb63550
Mihirpathakji <mihirgithub123456@gmail.com>
2026-01-25 01:28:31 +0530

    Time: 31 ms (95.69%) | Memory: 50.9 MB (19.24%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,37 +1,46 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-       
-        int n = nums.size();
-        int count=0;
-        vector<int>Prefix_sum(n,0);
-        Prefix_sum[0] = nums[0];
-
-        for(int i=1;i<n;i++)
-        {
-            Prefix_sum[i] = Prefix_sum[i-1] + nums[i];
-        } 
-
-        unordered_map<int,int>mp;
         
-        for(int j=0;j<n;j++)
-        {
-            //Case 1:
-            if(Prefix_sum[j] == k)
-            {
-                count++;
-            }   
+           long long n = nums.size();
+           long long count=0;
+           vector<long long> Prefix_sum(n,0);
 
-            int value  = Prefix_sum[j] - k;
+           Prefix_sum[0] = nums[0];
+           
+           for(int i=1;i<n;i++)
+           {
+                Prefix_sum[i] = Prefix_sum[i-1] + nums[i];
+           } 
 
-            if(mp.find(value)!=mp.end())
-            {
-                count += mp[value];
-            }
-            mp[Prefix_sum[j]]++;
-        }
-        return count;
+           //<Prefix_sum,Frequency_of_that_prefix_sum>
+           
+           unordered_map<long long ,long long> mp;
 
+           for(long long j = 0; j < n ; j ++)
+           {    
+                //Case 1:
+                //Prefix arr's  sum is itself  == k.
+                if(Prefix_sum[j] == k)
+                {
+                    count++;
+                }
+
+                //Case 2 :
+                //Any segment sum where segment is "not a prefix arr" has sum == k.
+                //Finding number of such segments: 
+                long long value = Prefix_sum[j] - k;
+                if(mp.find(value) != mp.end())
+                {
+                    count += mp[value];
+                }
+
+                mp[Prefix_sum[j]]++;
+
+           }
+            return count;
+            //TC:O(n)//.find() and insertion occurs in O(1) in an unordered_map.
+            //SC:O(n)//mp,Prefix_sum arr
 
     }
 };
