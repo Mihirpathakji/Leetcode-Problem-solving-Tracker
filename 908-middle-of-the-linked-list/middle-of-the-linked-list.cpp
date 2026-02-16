50543390485c0fde58547cd006deef0eff2547f0
Mihirpathakji <mihirgithub123456@gmail.com>
2026-02-16 22:27:01 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (25.91%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -14,22 +14,27 @@ public:
 
         ListNode* temp = head;
         long long n = 0;
+       
         while(temp != NULL)
         {
             temp = temp->next;
             n++;
-        }
+        }//O(n)
         //n = total number of nodes in the Linkedlist.
 
         long long mid = n/2;//2 or 3
         temp = head;
+        
         while(mid--)
         {
             temp = temp->next;
-        }
+        }//O(n/2)
 
         head = temp;
         return head;
+
+        //TC:O(n)
+        //SC:O(1)
 
     }
 };
