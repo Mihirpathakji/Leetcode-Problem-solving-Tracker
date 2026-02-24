1f01529502eec7c6fdbbb5fda74af6d79c4dfec3
Mihirpathakji <mihirgithub123456@gmail.com>
2026-02-25 00:44:00 +0530

    Time: 4 ms (51.38%) | Memory: 118 MB (82.30%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -8,44 +8,65 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+ListNode* Reverse_Linkedlist(ListNode* Head_Pointer)
+{
+    ListNode* Prev = NULL;
+    ListNode* curr = Head_Pointer;
+    ListNode* future = NULL;
+
+    while(curr)
+    {
+        future = curr->next;
+        curr->next = Prev;
+
+        Prev = curr;
+        curr = future;
+    }
+
+    return Prev;
+}
+
+
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
 
-        ListNode* temp = head;
-        vector<int>original;
-        
-        if(temp->next == NULL)
-        {
-            return true;
-        }
+        //Find the middle node:
 
+        ListNode* temp = head;
+        int no_nodes = 0;
+        
         while(temp != NULL)
         {
-            original.push_back(temp->val);//1 2
-            temp = temp->next;//2
+            no_nodes++;
+            temp = temp->next;
         }
-    
-        //original[]=[1 2 2 1]
-        int l = 0;
-        int r = original.size() - 1;
         
-        while(l < r)
+        int middle_node_cnt = no_nodes/2; //4/2 == 2
+        
+        temp = head;
+
+        while(middle_node_cnt)
         {
-            if(original[l] != original[r])
-            {
-                return false;
-            }
-            l++;
-            r--;
-            
+            temp = temp->next;
+            middle_node_cnt--;//1 0 
         }
 
+        //temp is at the head of the Linkedlist to be Reversed.
+
+        ListNode* PREV = Reverse_Linkedlist(temp);//100
+        temp = head;
+        
+        while(PREV != NULL )
+        {
+            if(PREV->val != temp->val)
+            return false;
+
+            PREV = PREV->next;
+            temp = temp->next;
+        }
         return true;
-
-
-        //TC:O(n)
-        //SC:O(n)
 
     }
 };
