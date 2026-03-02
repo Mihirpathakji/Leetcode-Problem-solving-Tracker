e62b25afce61b877fee9ad51065df8682f88807e
Mihirpathakji <mihirgithub123456@gmail.com>
2026-03-02 13:51:23 +0530

    Time: 0 ms (100.00%) | Memory: 30 MB (65.85%) - LeetSync

--- 1658-minimum-swaps-to-arrange-a-binary-grid/minimum-swaps-to-arrange-a-binary-grid.cpp
+++ 1658-minimum-swaps-to-arrange-a-binary-grid/minimum-swaps-to-arrange-a-binary-grid.cpp
@@ -1,55 +1,59 @@
 class Solution {
 public:
     int minSwaps(vector<vector<int>>& grid) {
-
-        int n = grid.size();
-        vector<int>need(n);
+        int n  = grid.size();
         int min_swaps = 0;
+        
+        vector<int>trailing_zeroes(n);
+        vector<int>need(n);
 
-        for(int row = 0; row < n; row++)
+        //need computation;
+        for(int i=0; i<n;i++)
         {
-            need[row] = n - row  -1;
+            need[i] = n-i-1;
         }
 
-        vector<int>trailing_zeroes(n);
-
-        for(int row = 0; row < n; row ++)
-        {  
-            int trailing_zeroes_cnt = 0;
-            for(int j = n -1; j >=0 ; j--)
+        //trailing_zeroes computation: computing Number of zeroes in continuous fashion from back
+        for(int i=0;i<n;i++)
+        {
+            int zeroes_from_back = 0;//0
+            for(int j=n-1;j>=0;j--)
             {
-                if(grid[row][j] == 0)
+                if(grid[i][j] == 0)
                 {
-                    trailing_zeroes_cnt++;
-                    if(j == 0)
+                    zeroes_from_back++;
+                    if(j == n-1)
                     {
-                        trailing_zeroes[row] = trailing_zeroes_cnt;
+                        trailing_zeroes[i] = zeroes_from_back;
                     }
-                }   
+                }
                 else
                 {
-                    trailing_zeroes[row] = trailing_zeroes_cnt;
+                    trailing_zeroes[i] = zeroes_from_back;
                     break;
                 }
             }
         }
 
-        for(int  i = 0; i < trailing_zeroes.size(); i ++)
+        for(int i=0;i<trailing_zeroes.size();i++)
         {
-            bool flag = false;
             if(trailing_zeroes[i] < need[i])
-            {
-                for(int j = i+1 ; j < trailing_zeroes.size() ; j++)
+            {//there is need to swap
+                bool flag = false;
+                for(int j = i+1;j<trailing_zeroes.size();j++)
                 {
-                    
                     if(trailing_zeroes[j] >= need[i])
                     {
                         flag = true;
                         min_swaps += j-i;
-                        while(j > i){
+                        //Bubble that trailing_zeroes[j] to trailing_zeroes[i] by adjacent swaps.
+
+                        while(j > i)
+                        {
                             swap(trailing_zeroes[j],trailing_zeroes[j-1]);
                             j--;
-                        }   
+                        }
+                        //For that i job is done .Now trailing_zeroes[i] >= need[i] no need to anything for that ith row now.
                         break;
                     }
                 }
@@ -57,11 +61,10 @@ public:
                 {
                     return -1;
                 }
-             
             }
         }
-
         return min_swaps;
-        
+        //TC:O(n^2)
+        //SC:O(n)
     }
 };
