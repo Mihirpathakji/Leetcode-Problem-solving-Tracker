0963e9987d0724650a5627da142a47e6777bdddf
Mihirpathakji <mihirgithub123456@gmail.com>
2026-03-02 16:40:28 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (19.86%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    bool isValid(string s) {
+
+        stack<char>st;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i] == '(' || s[i]=='[' || s[i]=='{')
+            {
+                st.push(s[i]);
+            }
+            else
+            {
+                if(s[i] ==')')
+                {   
+                    if(!st.empty() && st.top() == '(')
+                    {
+                        st.pop();
+                    }
+                    else
+                    {
+                        st.push(s[i]);
+                    }
+                }
+                else if(s[i] == ']')
+                {
+                    if(!st.empty() && st.top() == '[')
+                    {
+                        st.pop();
+                    }
+                    else
+                    {
+                        st.push(s[i]);
+                    }
+                }
+                else
+                {
+                    if(!st.empty() && st.top() == '{')
+                    {
+                        st.pop();
+                    }
+                    else
+                    {
+                        st.push(s[i]);
+                    }
+                }
+            }
+        }
+        return st.size() == 0;
+        
+        //TC:O(n)
+        //SC:O(n)
+
+    }
+};
