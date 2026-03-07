f96a03b7c0a9211baf31e9be45e7acb77f3a587e
Mihirpathakji <mihirgithub123456@gmail.com>
2026-03-07 22:15:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (46.67%) - LeetSync

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -4,12 +4,11 @@ public:
 
         int l = 0;
 
-        int no_consecutive_seg = 0;
-
         while(l < s.size() && s[l] =='1')
         {
             l++;
         }
+        
         //l -> '0'
 
         while(l < s.size() && s[l] == '0')
