225ccab2c1a336175124424cae0a4eac9dabb1ec
Mihirpathakji <mihirgithub123456@gmail.com>
2026-03-28 20:17:03 +0530

    Time: 640 ms (5.01%) | Memory: 154.6 MB (5.02%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+    
+    set<char>st(s.begin(),s.end());//' '
+
+    if(st.size() == 1)
+    {
+        //i.e only one  space type character was there.
+
+        return 1;//"    "->4 
+    }
+
+    unordered_map<char,int>mp;
+
+    long long  maximum_len = 0;
+    long long  current_count = 0;
+
+    //mp.find()-> If the element is already present in the map -> mp.find() returns an iterator which points to that element . If it is not present in the map  -> than it will return mp.end() 
+    
+    //O(n^2).
+    
+    for(int i = 0; i< (int)s.size();i++)
+    {
+        for(int j = i; j <(int) s.size() ; j++)
+        {
+            if(mp.find(s[j])!=mp.end())
+            {       
+                // element is present in the map.
+                //update the current len.
+                maximum_len = max(maximum_len,current_count);
+                current_count = 0;
+                mp.erase(mp.begin(),mp.end());
+                break;
+            }
+            else
+            {
+                mp[s[j]]++;//1 
+                current_count++;//1 2 
+            }
+        }
+    }
+    return maximum_len;
+    
+    }
+};
