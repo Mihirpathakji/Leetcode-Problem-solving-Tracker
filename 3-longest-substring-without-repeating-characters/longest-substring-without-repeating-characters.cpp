9ba12c4e555f2bab93429512dad9b55469921e76
Mihirpathakji <mihirgithub123456@gmail.com>
2026-03-28 22:03:53 +0530

    Time: 680 ms (5.01%) | Memory: 247.6 MB (5.02%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,55 +1,32 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-    
-    unordered_set<char>st(s.begin(),s.end());//' '
-
-    if(st.size() == 1)
-    {
-        //i.e only one  space type character was there.
-
-        return 1;//"    "->4 
-    }
-
-
-    long long  maximum_len = 0;
-    long long  current_count = 0;
-
-    //mp.find()-> If the element is already present in the map -> mp.find() returns an iterator which points to that element . If it is not present in the map  -> than it will return mp.end() 
-    
-    //O(n^2).
-    
-    for(int i = 0; i< (int)s.size();i++)
-    {
+        
         unordered_map<char,int>mp;
-        current_count = 0;
+        long long max_len = 0;
+        long long curr_len  = 0;
 
-        for(int j = i; j <(int) s.size() ; j++)
+        for(int i = 0 ;i < s.size();i++)
         {
-            if(mp.find(s[j])!=mp.end())
-            {       
-                // element is present in the map.
-                //update the current len.
-                maximum_len = max(maximum_len,current_count);
-                current_count = 0;
-                break;
-            }
-            else
+            unordered_map<char,int>mp;
+            curr_len = 0;
+            for(int j = i ; j < s.size() ; j++ )
             {
-                mp[s[j]]++;//1  1
-                current_count++;//1  2
+                if(mp.find(s[j])!= mp.end())
+                {
+                    break;
+                }
+                else
+                {
+                    curr_len++;
+                    mp[s[j]]++;
+                }
             }
+            max_len = max(max_len,curr_len);
         }
-        maximum_len  = max(maximum_len,current_count);
+
+        return max_len;
 
     }
 
-    maximum_len  = max(maximum_len,current_count);
-
-    return maximum_len;
-    
-    }
 };
-
-//SC : O(n)
-//TC : O(n^3) approx n^2;
