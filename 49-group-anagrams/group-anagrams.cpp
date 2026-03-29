94b8bef11cb0c5bc4ff0df0a19be125e662e109d
Mihirpathakji <mihirgithub123456@gmail.com>
2026-03-30 01:26:57 +0530

    Time: 18 ms (49.62%) | Memory: 27.2 MB (24.05%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-
-       vector<string>another_ans = strs;
-
-       for(int i = 0;i < another_ans.size();i++)
-       {
-            sort(another_ans[i].begin(),another_ans[i].end());
-       } 
-
-
-        unordered_map<string,vector<int>>mp;
+        
+        vector<string>another_ans = strs;//O(n*k)
         for(int i = 0;i<another_ans.size();i++)
         {
-            mp[another_ans[i]].push_back(i);//aet -> 0 1 3 
-            //ant -> 2  4
-            //5
+            sort(another_ans[i].begin(),another_ans[i].end());
+        }
+
+        //TC : O(n*klogk)
+        unordered_map<string,vector<int>>mp;
+        for(int i = 0; i< another_ans.size(); i++)
+        {
+            mp[another_ans[i]].push_back(i);
         }
 
         vector<vector<string>>final_ans;
-
         for(auto it : mp)
         {
-            vector<string> to_be_push;
-            for(int i = 0; i< it.second.size();i++)
+            vector<string>to_be_pushed;
+            for(int i = 0; i < it.second.size() ; i++)
             {
-                to_be_push.push_back(strs[it.second[i]]);
+                to_be_pushed.push_back(strs[it.second[i]]);
             }
+            final_ans.push_back(to_be_pushed);
 
-            final_ans.push_back(to_be_push);
         }
 
-        return final_ans;    
+        return final_ans;
+        //TC : O (n*klogk)
+        //SC : O(strs.size())
 
     }
 };
