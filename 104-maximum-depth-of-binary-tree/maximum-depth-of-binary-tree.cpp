ed16398bfe20b65097bed513fa516148b9a70814
Mihirpathakji <mihirgithub123456@gmail.com>
2026-04-03 18:55:33 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (47.32%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -9,53 +9,32 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
+   
     int maxDepth(TreeNode* root) {
-              
-        //Level order Traversal:
-        
-        queue<TreeNode*>q;
+
+        //Recursively:
 
         if(root == NULL)
         {
             return 0;
         }
+
+        //Calculate the Height of Left subtree from the root node 
+
+        int left_Subtree_Height = maxDepth(root->left);//1   0 
         
-        q.push(root);//[NULL]
-        
-        vector<vector<int>>levels;
-        
-        while(!q.empty())
-        {
-            vector<int>ans;
-            int size = q.size();//1 
-            
-            for(int i = 0; i < size; i++)
-            {//0 <1
-                //q = ""
-                root = q.front();//NULL
-                ans.push_back(root->val);//[1]
-                q.pop();//
-                
-                if(root->left)
-                {
-                    q.push(root->left);
-                }
-                
-                if(root->right)
-                {
-                    q.push(root->right);
-                }
-                
-            }
-            
-            levels.push_back(ans);//[[1] [2 3] [4 10 5 ] [6 7] ] //[1]
-        }
-        
-        return levels.size();
+        //Calculate the height of the right subtree from root node.
+
+        int right_Subtree_Height = maxDepth(root->right);// 0   2
+
+        //Total Height after including the root Node.
+
+        return max(left_Subtree_Height,right_Subtree_Height)+1;//1  1   2 
+
         //TC:O(N)
-        //SC:O(N)
-        
+        //SC:O(N)->AUXILLIARY SPACE.
     }
 };
