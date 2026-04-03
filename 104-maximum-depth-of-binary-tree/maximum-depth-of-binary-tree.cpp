0f91721a188aba247993ff4851f80d80df81522a
Mihirpathakji <mihirgithub123456@gmail.com>
2026-04-03 18:58:48 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (77.03%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -24,16 +24,18 @@ public:
 
         //Calculate the Height of Left subtree from the root node 
 
-        int left_Subtree_Height = maxDepth(root->left);//1   0 
+        int left_Subtree_Height = maxDepth(root->left);//1   0  // A->LEFT
         
         //Calculate the height of the right subtree from root node.
 
-        int right_Subtree_Height = maxDepth(root->right);// 0   2
+        int right_Subtree_Height = maxDepth(root->right);// 0   2 //  B->RIGHT
 
         //Total Height after including the root Node.
 
-        return max(left_Subtree_Height,right_Subtree_Height)+1;//1  1   2 
+        return max(left_Subtree_Height,right_Subtree_Height)+1;//1  1   2 // Root 
+        
 
+        //Postorder Traversal
         //TC:O(N)
         //SC:O(N)->AUXILLIARY SPACE.
     }
