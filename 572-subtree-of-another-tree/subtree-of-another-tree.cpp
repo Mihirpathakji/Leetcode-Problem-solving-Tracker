83ac1c118eb491be68ea54966f5596bf01f1e358
Mihirpathakji <mihirgithub123456@gmail.com>
2026-04-06 00:29:04 +0530

    Time: 0 ms (100.00%) | Memory: 28.9 MB (70.36%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -9,43 +9,41 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-
-             
 class Solution {
 public:
 
     bool isIdentical(TreeNode* p,TreeNode* q)
     {
-
         if(p == NULL || q == NULL)
         {
             return p == q;
         }
 
         return (p->val == q->val) && isIdentical(p->left,q->left) && isIdentical(p->right,q->right);
-    
     }
 
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
 
-        //Iterative -> NOT GOOD. Recursively.
+        //Find the top of the subRoot in the Main Tree
+        //Moment you found the same top node. Check whether both of those subtrees are identical/
+
+        //Edge Case :
 
         if(root == NULL || subRoot == NULL)
         {
             return root == subRoot;
         }
 
-        if(root->val == subRoot->val && isIdentical(root,subRoot))
+        if( (root->val == subRoot->val) && isIdentical(root,subRoot))
         {
-            return true;
+            return true;//isIdentical -> TC : O(M).M - Number of nodes in the subroot.
         }
-
-        //Not got the identical tree at the root node of the both .Hence checked at the Left and at the right.
-
+        
         return isSubtree(root->left,subRoot) || isSubtree(root->right,subRoot);
 
-        //Either we had got the identical subRoot in the left part of the currrent Node or in the right part of the current node.
+        //TC:O(N*M) N -> Number of nodes in Main Tree. M - Number of nodes in the subRoot.For each root in the Main Tree in worst case we will be checking that whole subRoot is identical with that node or not.
+
+        //SC: O(N) =M 
 
     }
 };
