bb40f5433a4dfb96498f2781772983d99f11c7e2
Mihirpathakji <mihirgithub123456@gmail.com>
2026-04-11 14:43:12 +0530

    Time: 0 ms (100.00%) | Memory: 34.4 MB (14.37%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -9,20 +9,22 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
 public:
-
     TreeNode* deleteNode(TreeNode* root, int key) {
 
-        if(root == NULL)
+
+        //We need the root node(Top node) of the final bst.
+        //Search The node to be deleted.
+
+        if(root == nullptr)
         {
-            return nullptr;
+            return NULL;//NULL.
         }
 
-        if(root->val < key)
+        else if(root->val < key)
         {
-            root->right =  deleteNode(root->right,key);
+            root->right = deleteNode(root->right,key);
         }
 
         else if(root->val > key)
@@ -32,72 +34,63 @@ public:
 
         else
         {
-            TreeNode* ans = root;//We got the Node to be deleted.
-            TreeNode* temp = ans;
+            //We got root == node to be deleted.
 
-            //Case 1 : No  child :-   
+            //Lets see that node to be deleted == root has no child or , one child , or 2 childs.
 
-            if(ans->left==NULL && ans->right==NULL)
+            if(root->left == nullptr && root->right==nullptr)
             {
-                delete ans;
-                ans = nullptr;
-                return nullptr;
-            }
-
-            //Case 2: Single Child :
-
-            else if(ans->left!=NULL && ans->right==NULL)
-            {   
-                TreeNode* temp  = ans->left;
-                delete ans;
-                ans = nullptr;
-    
-                return temp;
-            }
-
-            else if(ans->left==NULL && ans->right!=NULL)
-            {   
-                TreeNode* temp = ans->right;
-                delete ans;
-                ans = nullptr;
-                return temp;
-            }
-
-            //Case 3 :
-
-            //Node to be Deleted has two childs :
-
-            TreeNode* parent  = root;
-            TreeNode* child  = parent->left;
-
-
-            //Went to the LEFT subtree and Find it's rightmost.
-
-            while(child->right)
-            {   
-                parent = child;
-                child = child->right;
-            }   
-
-            
-            if(parent == root)
-            {   
-                child->right = root->right;
                 delete root;
-                return child;
-            }   
+                root = nullptr;
+            }
 
+            else if(root->right == nullptr && root->left !=nullptr)
+            {
+                TreeNode* temp = root->left;//1100
+                delete root;//1200
+                root = nullptr; 
+                return temp;//Will go to the right of node which comed ahead of the parent node after backtracking.
+            }
+            else if(root->right !=nullptr && root->left == nullptr)
+            {
+                TreeNode* temp = root->right;
+                delete root;
+                root = nullptr;
+                return temp; 
+            }
             else
             {
-                parent->right = child->left;
-                child->left = root->left;
-                child->right = root->right;
-                delete root;
-                return child; 
+                TreeNode* parent = root;
+                TreeNode* child  = root->left;//left subtree mei jao.Waha se rightmost find karo.
+
+                while(child->right)
+                {
+                    parent  = child;
+                    child = child->right;
+                }
+
+                if(parent == root)
+                {
+                    child->right = parent->right;   
+                    delete parent;
+                    parent = nullptr;
+                    return child;
+                }
+
+                else
+                {
+                    parent->right = child->left;
+                    child->right = root->right;
+                    child->left = root->left;
+                    delete root;
+                    root = nullptr;
+                    return child;
+                }
             }
+
         }
-
         return root;
-
+        //TC:O(Logn)
+        //SC:O(Logn)
     }
 };
