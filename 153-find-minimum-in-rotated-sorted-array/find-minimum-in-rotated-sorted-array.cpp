f41aee73fb8aaf3b9beb7e2e598aca2ffec71d95
Mihirpathakji <mihirgithub123456@gmail.com>
2026-05-15 23:55:32 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (10.85%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
-
     int findMin(vector<int>& nums) {
-        
+
         long long n = nums.size();
         long long low = 0;
-        long long high = n-1;
+        long long high = n - 1;
         long long mid = -1;
-
+    
         if(n == 1)
         {
             return nums[0];
@@ -15,30 +14,44 @@ public:
 
         while(low <= high)
         {
-            mid = low + (high-low)/2;
+            mid = low + (high - low)/2;
 
+            //Base case condition first.mid reaches at either of the corners.
+            
             if(mid == 0 && nums[mid] < nums[mid+1])
             {
-                return nums[mid];
+                return nums[mid];//nums[0]
             }
-            else if(mid == n- 1 && nums[mid] < nums[mid-1])
+
+            else if(mid == n-1 && nums[mid-1] > nums[mid])
             {
-                return nums[mid];
-            }
+                return nums[mid];//nums[n-1]
+            }   
+
+            //At any intermediate position.         
+
             else if(nums[mid] < nums[mid+1] && nums[mid] < nums[mid-1])
             {
                 return nums[mid];
             }
+
+            else if(nums[low] < nums[mid] && nums[mid] < nums[high])
+            {
+                return nums[low];
+            }
+
             else if(nums[mid] < nums[high])
             {
                 high = mid - 1;
             }
-            else
+
+            else if(nums[low] <= nums[mid])
             {
                 low  = mid + 1;
             }
-        }
-        return -1;
 
+        }
+        return -1;   
+        
     }
 };
