9b049c36940e10cddc7f7fd6b02ff355277aec0f
Mihirpathakji <mihirgithub123456@gmail.com>
2026-05-15 22:17:40 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (78.89%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,38 +1,44 @@
 class Solution {
 public:
+
     int findMin(vector<int>& nums) {
-        int n=nums.size();
-        int low=0;
-        int high=n-1;
-        int mid;
-        int ans=INT_MAX;
-        //Approach:We will use the Property of the Rotated sorted array:ATLEAST ONE OF THE  porition with respect to the mid must be sorted .Either left of the mid or the right of the mid or may be both.
+        
+        long long n = nums.size();
+        long long low = 0;
+        long long high = n-1;
+        long long mid = -1;
 
-        //->Key : "Identify" which portion of the nums is "sorted"."Use the property of that portion being sorted i.e  We know in sorted array the minimum value occur at the leftmost index only .Got that minimum value from the sorted part thereby eliminate that sorted part from which you had taken the minimum value .And search for the minimum  value in the another part only .Hence the search space gets reduced "
-
-        while(low<=high)
+        if(n == 1)
         {
-            mid=low+(high-low)/2;//1//2  3 
-            //1.Check if the left portion of the mid is sorted i.e [L,M]
-            if(nums[low]<=nums[mid])
+            return nums[0];
+        }
+
+        while(low <= high)
+        {
+            mid = low + (high-low)/2;
+
+            if(mid == 0 && nums[mid] < nums[mid+1])
             {
-                //left part is sorted ->took minimum from that left sorted part [L,M] which will be arr[l]
-                ans=min(ans,nums[low]);//11 11 11
-                //eliminate that taken(Left) search space->Move to the right .search in another part
-                low=mid+1;//2  3  4 
+                return nums[mid];
+            }
+            else if(mid == n- 1 && nums[mid] < nums[mid-1])
+            {
+                return nums[mid];
+            }
+            else if(nums[mid] < nums[mid+1] && nums[mid] < nums[mid-1])
+            {
+                return nums[mid];
+            }
+            else if(nums[mid] < nums[high])
+            {
+                high = mid - 1;
             }
             else
             {
-                //Left protion is not sorted than by the property of the "Rotated sorted array"->The Right part will be sorted .
-                //get the minimum element of the right part.[M,H]-> arr[Mid] is minimum in right part
-                ans=min(ans,nums[mid]);
-                //eliminate the taken right search space .i.e Move towards the left
-                high=mid-1;
+                low  = mid + 1;
             }
         }
-        return ans;
+        return -1;
 
-        //TC:O(log2(n))
-        //SC:O(1)
     }
 };
