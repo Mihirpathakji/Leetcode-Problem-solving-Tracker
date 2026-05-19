dca5ba162f7bb12cf67c429e2666d044de4e470f
Mihirpathakji <mihirgithub123456@gmail.com>
2026-05-19 17:19:30 +0530

    Time: 38 ms (12.47%) | Memory: 89.6 MB (8.40%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -5,6 +5,11 @@ public:
         //Compute the index of the next smaller element on right for each :
         vector<int>next_smaller_right(heights.size(),heights.size());
         stack<int>st1;
+        stack<int>st2;
+        
+        //Compute Index of next smaller on left for each element.
+        vector<int>next_smaller_left(heights.size(),-1);
+
         for(int i = 0; i< heights.size(); i++)
         {   
             while(!st1.empty() && heights[i] < heights[st1.top()])
@@ -13,28 +18,23 @@ public:
                 st1.pop(); 
             } 
             st1.push(i);
-        }
 
-        if(!st1.empty())
-        {
-            while(!st1.empty())
+            while(!st2.empty() && heights[heights.size()-i-1] < heights[st2.top()])
             {
-                st1.pop();
+                next_smaller_left[st2.top()] = heights.size() -i -1;
+                st2.pop();
             }
+            st2.push(heights.size()-i-1);
         }
 
-        //Compute Index of next smaller on left for each element.
-        vector<int>next_smaller_left(heights.size(),-1);
+        // if(!st1.empty())
+        // {
+        //     while(!st1.empty())
+        //     {
+        //         st1.pop();
+        //     }
+        // }
 
-        for(int i = heights.size()-1; i>= 0; i--)
-        {
-            while(!st1.empty() && heights[i] < heights[st1.top()])
-            {
-                next_smaller_left[st1.top()] = i;
-                st1.pop();
-            }
-            st1.push(i);
-        }
 
         //Area = Height*width.
         int maximum_area = INT_MIN;
