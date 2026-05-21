d9900ca7680a47f4f2c4c06d9d903c4d4c456cce
Mihirpathakji <mihirgithub123456@gmail.com>
2026-05-22 00:08:40 +0530

    Time: 411 ms (11.67%) | Memory: 170.3 MB (25.67%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
@@ -2,35 +2,38 @@ class Solution {
 public:
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
 
+        int ans = 0;//minimum possible length of the common prefix.
         unordered_map<string,int>mp;
 
-        // 1
-        // 10
-        // 100
-        // 1000
-        // 2
-        // 20
-        // 200
+        //Finding if the prefix of the any arr1[i] exists in arr2[] or not.
 
-        for(int i=0; i<arr1.size(); i++) {
+        for(int i = 0 ; i < arr1.size(); i++)
+        {
             string temp = to_string(arr1[i]);
-            for(int j=0; j<=temp.size(); j++) {
-                mp[temp.substr(0,j)] = 1;
+            //Hash All the Prefixes(0,j) where j  = 0 to size -1. of the arr1[i].
+            for(int j = 0;j <= temp.size(); j++)
+            {
+                mp[temp.substr(0,j)]++;
             }
         }
 
-        int maxi = 0;
-        for(int i=0; i<arr2.size(); i++) {
+        for(int i = 0 ; i< arr2.size() ; i++)
+        {
             string temp = to_string(arr2[i]);
-            for(int j=0; j<=temp.size(); j++) {
-                if(mp.find(temp.substr(0,j)) != mp.end()) {
-                    maxi = max(maxi,j);
+
+            for(int j = 0 ; j <= temp.size(); j++)
+            {
+                if(mp.find(temp.substr(0,j))!= mp.end())
+                {
+                    ans = max(ans,j);
                 }
             }
         }
 
-        return maxi;
+        return ans;
 
+        //TC :O() 
+        //SC :O()
 
     }
 };
