77e971fde8e0f366aa10c195ca66cc38210df400
Mihirpathakji <mihirgithub123456@gmail.com>
2026-05-23 17:21:45 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (5.20%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        
-        // Most optimal one solution for this problem : Tc : O(n)
 
-        // We will iterate through it till the Last element and check if the dip/Peak  occurs for only one time then it is correct Then it can be 
+        vector<int>original = nums;
+        sort(original.begin(),original.end());
+        vector<int>nums2 = nums;
 
-        int n=nums.size(); 
-        int dip=0;
-        for(int i=0;i<n-1;i++)
+        int k = nums.size();//
+
+        while(k)
         {
-            if(nums[i]>nums[(i+1)])
+            
+            for(int i =0; i < nums.size(); i++)
             {
-                // there is one downfall founded.
-                dip++; 
+                nums2[(i+1)%nums2.size()] = nums[i];
             }
-        
+            
+            if(nums2 == original)
+            {
+                return true;
+            }
+            k--;
+            nums=nums2;
+            
         }
-        if(nums[n-1]>nums[0])
-        {
-            dip++;
-        }
 
-
-        return dip<=1; 
-
+        return false;
 
     }
 };
