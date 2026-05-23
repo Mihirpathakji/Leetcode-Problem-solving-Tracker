fd2909376a66d684ed00b2cf7110dac276d761d9
Mihirpathakji <mihirgithub123456@gmail.com>
2026-05-23 17:52:23 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (5.20%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -3,27 +3,31 @@ public:
     bool check(vector<int>& nums) {
 
         vector<int>original = nums;
-        sort(original.begin(),original.end());
+        sort(original.begin(),original.end());//nlogn
         vector<int>nums2 = nums;
 
-        int k = nums.size();//
+        int k = nums.size();//n
 
         while(k)
         {
             
             for(int i =0; i < nums.size(); i++)
             {
-                nums2[(i+1)%nums2.size()] = nums[i];
+                nums2[(i+1)%nums2.size()] = nums[i];//2nd time rortation che i.e nums should be now the nums rotated once .so that the nums2 will be nums after twice rotations.
             }
             
             if(nums2 == original)
             {
                 return true;
             }
+
             k--;
-            nums=nums2;
-            
+
+            nums = nums2;
+
         }
+        
+        //TC : O(nlogn) + O(n*(n+2*n)) == O(nlogn) + O(n*(3*n)) == O(nlogn) + O(n^2)
 
         return false;
 
