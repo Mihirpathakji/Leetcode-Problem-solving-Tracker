992823ae3a1c4e7c70f430b048100a0c991522da
Mihirpathakji <mihirgithub123456@gmail.com>
2026-05-24 12:33:19 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (76.86%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -9,34 +9,16 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
 public:
-   
     int maxDepth(TreeNode* root) {
-
-        //Recursively:
 
         if(root == NULL)
         {
             return 0;
         }
 
-        //Calculate the Height of Left subtree from the root node 
-
-        int left_Subtree_Height = maxDepth(root->left);//1   0  // A->LEFT
+        return max(maxDepth(root->left),maxDepth(root->right)) + 1;
         
-        //Calculate the height of the right subtree from root node.
-
-        int right_Subtree_Height = maxDepth(root->right);// 0   2 //  B->RIGHT
-
-        //Total Height after including the root Node.
-
-        return max(left_Subtree_Height,right_Subtree_Height)+1;//1  1   2 // Root 
-        
-
-        //Postorder Traversal
-        //TC:O(N)
-        //SC:O(N)->AUXILLIARY SPACE.
     }
 };
