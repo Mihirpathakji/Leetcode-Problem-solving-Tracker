56c8679c888b417b8d22a25f54b1b2301f015a67
Mihirpathakji <mihirgithub123456@gmail.com>
2026-05-28 00:28:49 +0530

    Time: 95 ms (24.16%) | Memory: 25.5 MB (26.97%) - LeetSync

--- 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
@@ -2,61 +2,48 @@ class Solution {
 public:
     int numberOfSpecialChars(string word) {
 
-        unordered_map<char,int>Capitals;
+        //For the smaller case elements just push their last index.Since,if their last index is before the first occurance of the Capital of it i.e every occurance of the small_case characters is before the first occurance of the upper case of it.
 
-        for(int i = 0 ; i < word.size(); i++)
-        {
-            if(word[i] >= 'A' && word[i] <= 'Z')
+        unordered_map<char,int>small_upper_both;
+
+        //inserting the last indexes of the small_case letters.
+
+        //Backward loop.
+
+        for(int i = word.size()-1;i >= 0 ;i--)
+        {   
+            //small case : 
+            if(word[i] >= 95 && word[i] <= 122 && (small_upper_both.find(word[i]) == small_upper_both.end()))
             {
-                if(Capitals.find(word[i]) == Capitals.end())
-                {
-                    Capitals[word[i]] = i;//2
-                }
+                small_upper_both[word[i]] =  i;//last index.
             }
         }
 
-        unordered_map<char,vector<int>>small_case;
-        unordered_set<char>small_case1;
+        //Inserting First index Upper case letters into map.
+
+        //Forward loop : 
 
         for(int i = 0; i< word.size(); i++)
         {
-            if(word[i] >= 'a' && word[i] <= 'z')
+            if(word[i] >= 'A' && word[i] <= 'Z' && small_upper_both.find(word[i]) == small_upper_both.end())
             {
-                small_case[word[i]].push_back(i);
-            }
-        }
-
-        for(int i = 0; i< word.size(); i++)
-        {
-            if(word[i] >= 'a' && word[i] <= 'z')
-            {
-                small_case1.insert(word[i]);
+                small_upper_both[word[i]] = i;
             }
         }
 
         int count = 0;
-
-        for(auto it : small_case1)
+        for(auto it : small_upper_both)
         {
-            bool flag = false;
-            if(Capitals.find(it - 32)!=Capitals.end())
+            //it = {key,value}. it,first = key, it.second = value.
+            if(it.first >= 'A' && it.first <= 'Z')
             {
-                vector<int>temp = small_case[it];//0,1
-                //Checking every_occurance of the small_case is before upper_case
-                for(int i = 0 ; i < temp.size(); i++)
+                if(small_upper_both.find(it.first+32)!=small_upper_both.end() && small_upper_both[it.first + 32] < small_upper_both[it.first])
                 {
-                    if(temp[i] >= Capitals[it-32])
-                    {
-                        flag = true;
-                        break;
-                    }
-                }
-                if(!flag)
-                {
-                    count++;//1
+                    count++;//
                 }
             }
         }
         return count;
+
     }
 };
