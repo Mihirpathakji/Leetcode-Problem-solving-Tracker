c6138797e01ef2362cebf70c36f97b2271ebb821
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-02 23:21:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (33.69%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    
+    int climbStairs(int n) {
+
+        //Bottom Up Approach : 
+
+        //1.Insert the base case into the dp.
+
+        vector<int>dp(n+1,-1);
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        if(n <= 1)
+        {
+            return 1;
+        }
+
+        //2.Move upwards through for loop.
+
+        for(int i = 2;i <= n; i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+
+        //TC : O(n)
+        //SC : O(n)
+        
+    }
+};
