919e645c0ca92a5fc73ef298ec040d329ea5589e
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-02 17:44:16 +0530

    Time: 3 ms (62.17%) | Memory: 8.2 MB (17.93%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
 
-    int myfib(int n ,vector<int>& dp)
-    {
-        
-        if(n <= 1)
-        {
-            return n;
-        }
-
-        //Already calculated use it:         
-        if(dp[n]!=-1)
-        {
-            return dp[n];
-        }
-    
-        return dp[n] = fib(n-1) + fib(n-2);
-    }
-
     int fib(int n) {
 
-        vector<int>dp(n+1,-1);
+        //Bottom - Up approach 
 
-        return myfib(n,dp);
+        vector<int>dp(n+1);
 
+        //Compute base case first.
+       
+        dp[0] = 0;
+        
+        if(n >=1) 
+        dp[1] = 1;
+
+        //Than go upwards.Replace func by dp[i]
+
+        for(int i = 2; i <= n ; i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        } 
+
+        return dp[n];
+    
     }
+
 };
