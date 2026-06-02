7dc829fca24df401ba06a6aee45920386b81dfcc
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-02 23:52:24 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (70.14%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    
     int climbStairs(int n) {
 
-        //3.Bottom Up Approach : 
+        //4. Space Optimization : 
 
-        //1.Insert the base case into the dp.
+        //Here the no. of distinct ways to climb the nth stairs depends on the number of distinct ways to climb the (n-1)th stair and (n-2)th stair only.
 
-        vector<int>dp(n+1,-1);
-
-        dp[0] = 1;
-        dp[1] = 1;
+        //since every ith depend only on (i-1)th and (i-2)th only.No need to create a extra vector.We can store these two values of climbStairs[i-1] and climbStairs[i-2] in a single variable only.    
 
         if(n <= 1)
         {
-            return 1;
+            return 1;//f(0),f(1)
         }
 
-        //2.Move upwards through for loop.
+        int prev1 = 1;//f(0)=1
+        int prev2 = 1;//f(1)=1
+        int ans = 0;
 
-        for(int i = 2;i <= n; i++)
+        for(int i = 0; i <= n-2 ;i++)
         {
-            dp[i] = dp[i-1] + dp[i-2];
+            ans = prev1 + prev2;
+            prev1 = prev2;//first = second
+            prev2 = ans;//second = third. 
         }
 
-        return dp[n];
+        return ans;
 
         //TC : O(n)
-        //SC : O(n)
-        
+        //SC : O(1)
+
     }
 };
