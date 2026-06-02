17cff4236e340e4b12b4178a2210adf8a91d4539
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-02 12:36:03 +0530

    Time: 10 ms (42.84%) | Memory: 7.8 MB (38.41%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
     int fib(int n) {
-      
-        if(n==0||n==1)return n;
 
-        else
+        if(n == 0)
         {
-            // the third,second and the first grows very Largely in the fibonacci numbers hence you need to give them the data type long long int to avoid integer overflow  .
-
-        long long int first=0;
-        long long int second=1;
-
-        long long int third=0;
-
-        for(int i=2;i<=n;i++)
+            return 0;
+        }
+        if(n == 1)
         {
-            third=first+second;
-            first=second;
-            second=third;
+            return 1;
         }
-        // i== n after the for loop ended the i was == n . 
-
-        return third;
-
-        }
-    
+        return fib(n-1)+fib(n-2);
+        
     }
 };
