2f3b7fa660ae7c3228f14bd5f73a92f73b67cb31
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-03 19:04:23 +0530

    Time: 14 ms (31.19%) | Memory: 238.1 MB (66.06%) - LeetSync

--- 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.cpp
+++ 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.cpp
@@ -10,33 +10,35 @@ public:
         for(int i = 0 ; i  < landStartTime.size(); i ++)
         {
             land_ride_complete_time = min(land_ride_complete_time,landStartTime[i] + landDuration[i]);
-        }
+        }//44
 
         //for this land_ride_complete_time -> see the minimum total time we could make in total while taking the all possible water routes.
 
         int can_be_ans = INT_MAX;
         for(int i = 0;i < waterStartTime.size(); i++)
         {
-            can_be_ans = min(can_be_ans,max(land_ride_complete_time,waterStartTime[i]) + waterDuration[i]);
+            can_be_ans = min(can_be_ans,max(land_ride_complete_time,waterStartTime[i]) + waterDuration[i]);//44+91 = 135  54+71 = 125  125
         }
 
-        final_ans = min(final_ans,can_be_ans);
+        final_ans = min(final_ans,can_be_ans);//125
+
+        //Path2 : water -> land
 
         int water_ride_complete_time = INT_MAX;
         for(int i = 0 ; i  < waterStartTime.size(); i ++)
         {
             water_ride_complete_time = min(water_ride_complete_time,waterStartTime[i] + waterDuration[i]);
-        }
+        }//97
 
         //for this water time find the minimum time we could make in total while taking the all possible land routes.
 
         can_be_ans = INT_MAX;
         for(int i = 0; i < landStartTime.size(); i++)
         {
-            can_be_ans = min(can_be_ans,max(water_ride_complete_time,landStartTime[i]) + landDuration[i]);
+            can_be_ans = min(can_be_ans,max(water_ride_complete_time,landStartTime[i]) + landDuration[i]);//97+42 = 139 // 97+30 = 127 -> 127
         }
 
-        final_ans = min(final_ans,can_be_ans);
+        final_ans = min(final_ans,can_be_ans);//min(125,127) = 125
 
         return final_ans;
 
