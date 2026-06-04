b6811bf081d80340606c1db47714f0d5978a0369
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-05 01:21:53 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (71.37%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -2,27 +2,23 @@ class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
 
-        //3.Bottom Up Approach : 
-        //1.start inserting the base cases into the dp :
+        //4.Space Optimization : 
 
         int n = cost.size();
-
-        //3.for loop replace functions by dp : 
-
         int first = 0;
         int second = 0;
         int third = -1;
 
         for(int i = 2; i <= n; i++)
         {
-            //cost to went to ith step:
+            //cost to reach ith step:
             third = min(cost[i-1] + second, cost[i-2] + first);
             first = second;
             second = third;
         }
         
         return third;
-        
+
         //TC : O(n)
         //SC : O(n)
 
