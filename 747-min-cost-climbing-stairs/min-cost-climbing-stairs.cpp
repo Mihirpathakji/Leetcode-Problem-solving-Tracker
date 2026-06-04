fc5a3efed3ebe713844289ffcd3738ed43178c31
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-05 01:40:08 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (71.37%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -5,13 +5,18 @@ public:
         //4.Space Optimization : 
 
         int n = cost.size();
-        int first = 0;
-        int second = 0;
+
+        //Base case :         
+
+        int first = 0;//f(0) -> minimum cost to reach 0th step.
+        int second = 0;//f(1) -> minimum cost to reach 1th step.
+
         int third = -1;
 
         for(int i = 2; i <= n; i++)
         {
             //cost to reach ith step:
+            //cost[2] = min(cost[1]+ )
             third = min(cost[i-1] + second, cost[i-2] + first);
             first = second;
             second = third;
