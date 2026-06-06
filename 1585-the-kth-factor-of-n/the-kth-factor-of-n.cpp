67027b7a16b720584a2acc57ec39e89bda8cf0e4
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-07 01:11:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (41.27%) - LeetSync

--- 1585-the-kth-factor-of-n/the-kth-factor-of-n.cpp
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.cpp
@@ -6,14 +6,10 @@ public:
         for(int i = 1;i <= n;i++)
         {
             if(n % i == 0)
-            {//i is factor of n
+            {
                 factors_of_n.push_back(i);
             }
         }
-        
-        // n = 4 . k = 4 -> [1,2,4] -> 3 factors are only there of n 
-
-        //If total no.of factors of n are less than asked no. of factor i.e that no. factor doesn't exists. 
 
         if(factors_of_n.size() < k)
         {
