19b9cd7ffb87e45b8d1face4f5cad2f26b2711b2
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-11 15:42:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (80.86%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -5,9 +5,9 @@ public:
         //Optimal :
 
         long long int count1 = 0;
-        while(n )
+        while(n)
         {
-            n = (n & (n-1));
+            n = (n & (n-1));//Keep on extracting and removing the right most 1.
             count1++; 
         }
 
