222ee98c971be30bb12ef8465ee2a29314602971
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-11 14:53:55 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (48.48%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -2,27 +2,15 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
 
-       //Brute Force :
+        //Bit Manipulation :
+        if(n == 0)
+        {
+            return 0;
+        }
+        return ( (n & (n-1LL) )== 0);
 
-       vector<int>binary;
-       while(n > 0)
-       {
-            binary.push_back(n%2);
-            n/=2;//8421
-       }
-       
-       //Hint : If a number is power of two -> It contains Exactly 1 set bit in it's binary representation.
-
-       int countof1 = 0;
-       for(int i = 0 ; i< binary.size(); i++)
-       {
-            if(binary[i] == 1)
-            {
-                countof1++;
-            }
-       }
-
-        return countof1 == 1;
-
+        //TC : O(1)
+        //SC : O(1)
+        
     }
 };
