4a382d4534b81cca89c12fb74229de12282b8a55
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-11 17:07:48 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (24.88%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -2,57 +2,22 @@ class Solution {
 public:
     int minBitFlips(int start, int goal) {
 
-        //Convert start and goal to binary.
-        
-        vector<int>start_binary;
-        while(start > 0)
-        {
-            int remainder = start%2;
-            start_binary.push_back(remainder);
-            start = start >> 1;
+        int target = start^goal;
+
+        //answer is Number of set bits in target.
+       
+        long long int count = 0;
+       
+        while(target)
+        {   
+            target = (target & (target-1));//Keep on Removing rightmost zeroes.
+            count++;
         }
 
-        vector<int>goal_binary;
-        while(goal > 0)
-        {
-            int remainder = goal%2;
-            goal_binary.push_back(remainder);
-            goal = goal >> 1;
-        }
+        return count;
 
-        if(start_binary.size() > goal_binary.size())
-        {
-            while(goal_binary.size()!=start_binary.size())
-            {
-                goal_binary.push_back(0);//4 5 
-            }
-        }
-        
-        else if(goal_binary.size() > start_binary.size())
-        {
-            while(start_binary.size()!=goal_binary.size())
-            {
-                start_binary.push_back(0);
-            }
-        }
-
-        reverse(start_binary.begin(),start_binary.end());   
-        reverse(goal_binary.begin(),goal_binary.end());
-
-        int min_flips = 0;
-
-        for(int i = 0 ;i < goal_binary.size();i++)
-        {
-            if( (goal_binary[i] ^ start_binary[i]) == 1)
-            {   
-                min_flips++;
-            }
-        }
-
-        return min_flips;
-
-        //TC : O(max(log(start),log(end)))
-        //SC : O(2*(max(log(start),log(end)))
+        //TC : O(31)
+        //SC : O(1)
         
     }
 };
