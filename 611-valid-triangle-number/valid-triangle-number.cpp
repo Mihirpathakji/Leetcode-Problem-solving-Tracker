d38acd269959f8d74a565cdbb7918075deea3aa3
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-13 02:55:09 +0530

    Time: 31 ms (67.59%) | Memory: 16.5 MB (75.06%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.cpp
+++ 611-valid-triangle-number/valid-triangle-number.cpp
@@ -7,33 +7,39 @@ public:
         
         sort(nums.begin(),nums.end());//nlogn
 
-        //For every fixed value of c.Find all valid pairs of {a,b,c->fixed}.With help of two pointers.as a and b 
+        //a -> index of shortest side.
+        //b -> index of middle side.
+        //c -> index of Largest side.
+
+        //For every fixed value of c(Largest side).Find all valid pairs of {a,b,c->fixed}.With help of two pointers.as a and b 
 
         for(int c = n-1; c >= 2; c--)
         {
-            int a = 0;
-            int b = c - 1; 
+            int a = 0;//index of the shortest side.not value
+            int b = c - 1; //index of middle side.not value.  
+
+            //Fixed the largest side and the middle side.And count all possible valid shortest sides.
 
             while(a < b)
             {
-                if((nums[a] + nums[b]) > nums[c])
+                if( (nums[a] + nums[b]) > nums[c])
                 {
-                    counts += (b-a);//there can be (b-a) valid positions for the number a.with that b side as the middle side.Now With line count += (b-a) we had already counted all the triangles that can be formed using b as it's middle side.Now Count all other possible triangles with smaller b's as their middle side.
+                    counts += (b-a);//valid triplets.
+                    
+                    //there can be (b-a) valid positions for the shortest side a.with that b side as the middle side.Now With line count += (b-a) we had already counted all the triangles that can be formed using b as it's middle sid and all possible shorter side for that middle side .Now Count all other possible triangles with smaller b's as their middle side.
                     b--;
                 }
                 else
                 {
-                    a++;
+                    a++;//12  
                 }
             }
-
         }
 
         return counts;
 
         //TC : O(nlogn) + O(n^2)
         //SC : O(1)
-
 
     }
 };
