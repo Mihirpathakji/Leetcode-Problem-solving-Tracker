4c7beb595ebf3dd035b4f1b1fa1664e3605ebd87
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-14 01:30:27 +0530

    Time: 3 ms (5.07%) | Memory: 8.7 MB (11.73%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.cpp
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -1,54 +1,40 @@
 class Solution {
 public:
-
     bool backspaceCompare(string s, string t) {
 
-        for(int i = 0;i < s.size(); i++)
-        {//s.size()==5
-            if(s[i] == '#')
-            {
-                s.erase(s.begin()+i);//erasing a single element from the string.Than you need to give the iterator of that single element. 
+        stack<char>ss;//for s
+        stack<char>st;//for t
 
-                if(i!=0)
-                {
-                    s.erase(s.begin()+i-1);//O(N)
-                    i = i-2;//-1
-                }
-                else
-                {
-                    i--;//-1
-                }
-            }   
-            //0
+        for(int i = 0 ; i< s.size(); i++)
+        {
+            if( s[i] != '#' )
+            {   
+                ss.push(s[i]);
+            }
+            else
+            {
+                if(!ss.empty())
+                ss.pop();
+            }
         }
-        //O(N^2)
 
         for(int i = 0;i < t.size(); i++)
         {
-            if(t[i] == '#')
+            if( t[i] != '#' )
             {
-                t.erase(t.begin()+i);//erasing a single element from the string.Than you need to give the iterator of that single element. 
-
-                if(i!=0)
-                {
-                    t.erase(t.begin()+i-1);//O(N)
-                    i = i-2;//-1
-                }
-                else
-                {
-                    i--;//-1 -1
-                }
-            }   //i=0 0
+                st.push(t[i]);
+            }
+            else
+            {   
+                if(!st.empty())
+                st.pop();
+            }
         }
-        //O(N^2)
 
-        //Removing single character from string ->s.erase(iterator) == s.erase(s.begin()+i); 
+        return ss == st;
 
-        return s == t;
-
-        //TC : O(s^2 + t^2).Since .erase in a string takes O(n)tc.Since all the string elements "Shifts" to the left on one deletion.
-
-        //SC : O(1)
-
+        //TC : O(s+t)
+        //SC : O(s+t)
+        
     }
 };
