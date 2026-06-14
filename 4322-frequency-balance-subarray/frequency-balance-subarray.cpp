83e7b522301a222247159a7741bc1f1c8bed667f
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-15 02:16:11 +0530

    Time: 1515 ms (20.00%) | Memory: 473 MB (10.00%) - LeetSync

--- 4322-frequency-balance-subarray/frequency-balance-subarray.cpp
+++ 4322-frequency-balance-subarray/frequency-balance-subarray.cpp
@@ -1,46 +1,69 @@
 class Solution {
 public:
     int getLength(vector<int>& nums) {
+
         int n = nums.size();
-        int max_len = 0;
+        int max_len = INT_MIN;
+        //Brute Force for every subarray: 
 
-        for (int i = 0; i < n; i++) {
-            unordered_map<int, int> mp;          // Tracks: element -> frequency
-            unordered_map<int, int> freq_counts; // Tracks: frequency -> how many elements have it
+        for(int i = 0; i < n; i++)
+        {
+            unordered_map<int,int>mp;
 
-            for (int j = i; j < n; j++) {
-                int val = nums[j];
-                int old_freq = mp[val];
-                int new_freq = old_freq + 1;
+            unordered_map<int,int>frequency_count;//stores the frequency and number of elements with that frequency.
 
-                // 1. Update element frequency
-                mp[val] = new_freq;
+            for(int j = i;j < n;j++)
+            {
+                int old_freq = mp[nums[j]];
+                int new_freq = old_freq + 1; 
 
-                // 2. Safely update the frequency-of-frequencies map in O(1)
-                if (old_freq > 0) {
-                    freq_counts[old_freq]--;
-                    if (freq_counts[old_freq] == 0) {
-                        freq_counts.erase(old_freq); // Keep map size accurate
+                mp[nums[j]] = new_freq;   
+
+                if(old_freq > 0)
+                {
+                    frequency_count[old_freq]--;//Number of the elements with that old frequency decreases.
+                    if(frequency_count[old_freq] == 0)
+                    {
+                        frequency_count.erase(old_freq);
                     }
                 }
-                freq_counts[new_freq]++;
 
-                // 3. Perform O(1) balance checks directly using map sizes
-                if (mp.size() == 1) {
-                    max_len = max(max_len, j - i + 1);
-                } 
-                else if (freq_counts.size() == 2) {
-                    auto it = freq_counts.begin();
-                    int f1 = it->first;
-                    int f2 = next(it)->first; // Safely jump to the second frequency
+                frequency_count[new_freq]++;
 
-                    if (f1 == 2 * f2 || f2 == 2 * f1) {
-                        max_len = max(max_len, j - i + 1);
+                //If there are only 1 distinct elements.
+
+                if(mp.size() == 1)
+                {
+                    max_len = max(max_len,j-i+1);
+                }
+
+                //More than 1 distinct but each has frequency f or 2*f.
+
+                //Number of distinct frequnecies should be exactly 2
+                else if(frequency_count.size() == 2)
+                {
+                    //There are only 2 distinct keys i.e frequency.
+
+                    auto it =  frequency_count.begin();//In Map ->The iterators acts like pointers.Hence,must use '->'   *. is not allowed to access their data. 
+
+                    int first = (it->first);//1st frequency ->f or 2*f
+                   
+                    it++;
+                    int second = it->first;
+
+                    if(first == 2*second || second ==2*first)
+                    {
+                        max_len = max(max_len,j-i+1);
                     }
                 }
-            }
+            }            
+
         }
 
         return max_len;
+
+        //TC : O(n^2)
+        //SC : O(n)
+        
     }
 };
