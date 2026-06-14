9adf6d6b9e059eaf779c4d09d99ac4ea651a4168
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-14 13:13:03 +0530

    Time: 3 ms (50.41%) | Memory: 30.4 MB (31.04%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,38 +1,21 @@
 class Solution {
 public:
-    vector<int> sortedSquares(vector<int>& arr) {
+    vector<int> sortedSquares(vector<int>& nums) {
 
-    int n=arr.size();
-    int i=0;
-    int j=n-1;
-    vector<int>ans(n);
-    int k=n-1;//last index 
+    //Brute force :
 
-    while(i<j)
+    for(int i = 0; i < nums.size();i++)
     {
-        if(arr[i]*arr[i]<arr[j]*arr[j])
-        {
-            ans[k]=(arr[j]*arr[j]);
-            j--;
-        }
-        
-        else if(arr[i]*arr[i]>arr[j]*arr[j])
-        {
-            ans[k]=(arr[i]*arr[i]);
-            i++;
-        }
-        else
-        {
-            ans[k]=(arr[i]*arr[i]);
-            i++;
-        }
-        k--;
-    }
-    ans[k]=(arr[i]*arr[i]);
-    return ans;
-    
-    //100 16 9 1 0 ->result of push_back better is kept an index variable . 
-    
-    }
+        nums[i] = (nums[i]*nums[i]);
+    }    
 
+    sort(nums.begin(),nums.end());
+
+    return nums;
+
+    //TC : O(nlogn)
+    //SC : O(1)
+
+        
+    }
 };
