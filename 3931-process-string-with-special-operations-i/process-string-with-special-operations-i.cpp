b3e90c5ab4e9b19fd6723924d74517171141824e
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-16 12:36:53 +0530

    Time: 0 ms (100.00%) | Memory: 39.8 MB (42.05%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -2,79 +2,36 @@ class Solution {
 public:
     string processStr(string s) {
 
-        int n = s.length(); 
-        stack<char>st;//g
-        stack<char>rev;
-        for(int i = 0;i<n;i++)
+        int n = s.length();    
+        string result;
+
+        for(int i = 0 ; i < n;i++)
         {
-            if(s[i] == '*')
+            //case1 : Lowercase english letters.
+            if(s[i] >= 'a' && s[i] <='z')
+            {   
+                result.push_back(s[i]);
+            }
+            else if(s[i] == '*')
             {
-                if(!st.empty())
+                if(!result.empty())
                 {
-                    st.pop();
+                    result.pop_back();
                 }
             }
             else if(s[i] == '#')
             {
-                stack<char>temp;
-                temp = st;//a
-
-                stack<char>temp2;
-                while(!temp.empty())
-                {
-                    temp2.push(temp.top());
-                    temp.pop();
-                }
-                //O("cba").
-
-                while(!temp2.empty())
-                {
-                    st.push(temp2.top());//[abcabc]
-                    temp2.pop();
-                }
-
-            }
-            else if(s[i] == '%')
-            {   
-                while(!st.empty())
-                {
-                    rev.push(st.top());//g
-                    st.pop();
-                }
-
-                st = rev;//g
-
-                //Empty() the rev for another % comes in future.
-                
-                while(!rev.empty())
-                {
-                    rev.pop();//
-                }
-            }
-            else
+                result += result;
+            }   
+            else if(s[i] =='%')
             {
-                st.push(s[i]);
+                reverse(result.begin(),result.end());
             }
         }
 
-        //stack to string.
-        //st = "".st.top() = 'ab'.
+        return result;
 
-        //We can access st.top() in stack.
-        //s = 'ab'
-
-        string ans;
-        while(!st.empty())
-        {
-            ans.push_back(st.top());//g
-            st.pop();
-        }
-
-        reverse(ans.begin(),ans.end());
-        return ans;
-
-        //TC : O(n*3*n)
-        //SC : O(n)
-
+        //TC : O(1)
+        //SC : O(1)
     }
 };
