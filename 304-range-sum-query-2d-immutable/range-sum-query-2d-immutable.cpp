37a936697a29638a2cb80886a37a56432a66e4a4
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-20 02:04:22 +0530

    Time: 18 ms (80.48%) | Memory: 149 MB (97.86%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
@@ -3,16 +3,16 @@ public:
 
 // Make a global vector. 
 
-    vector<vector<int>>prefix_matrix1;
+    vector<vector<int>>prefix_matrix;
 
     NumMatrix(vector<vector<int>>& matrix) {
 
-        //Compute the Prefix_Matrix:
+        //Compute the Prefix_Matrix:-> - topleft.
 
         int m = matrix.size();//rows.
         int n = matrix[0].size();//cols.
 
-        vector<vector<int>>prefix_matrix(m,vector<int>(n));
+        prefix_matrix = vector<vector<int>>(m,vector<int>(n));
 
         for(int i = 0; i < m ;i++)
         {
@@ -25,26 +25,31 @@ public:
                 prefix_matrix[i][j] = matrix[i][j] + top + left - topleft;
             }
         }
+        
+        //O(m*n)
 
-        prefix_matrix1 = prefix_matrix;
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
 
-        //answering Queries:
+        //answering Queries: -> + topleft.
         //+topleft
 
         //tc : O(1).
 
-        int total = prefix_matrix1[row2][col2];
-        int top  = (row1 > 0) ? prefix_matrix1[row1-1][col2] : 0;
-        int left = (col1 > 0) ? prefix_matrix1[row2][col1-1] : 0;
-        int topleft = (row1>0 && col1>0) ? prefix_matrix1[row1-1][col1-1] : 0;  
+        int total = prefix_matrix[row2][col2];
+        int top  = (row1 > 0) ? prefix_matrix[row1-1][col2] : 0;
+        int left = (col1 > 0) ? prefix_matrix[row2][col1-1] : 0;
+        int topleft = (row1>0 && col1>0) ? prefix_matrix[row1-1][col1-1] : 0;  
 
         //after all q queries tc : O(q*1)).
 
         return total - left - top + topleft;
     }
+
+    //tc : O(m*n + q)
+    //sc : O(m*n)
+
 };
 
 /**
