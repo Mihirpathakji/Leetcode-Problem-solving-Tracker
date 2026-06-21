024fc8eaa2c9ee575e159a5d3e971755cc371e9b
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-21 22:34:37 +0530

    Time: 18 ms (29.93%) | Memory: 38.6 MB (25.42%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,51 +1,34 @@
 class Solution {
 public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
 
-//This func gives no. of subarrays with sum <= goal.
-
-    int subarrays(vector<int> nums, int goal)
-    {
-
-        if(goal == -1)
-        {
-            return 0;
-        }
-
+        //No. of Subarray with sum equals k : Prefix sum + Hashmap.
+        
         int n = nums.size();
-        int i = 0;
-        int j = 0;
+        int prefix_sum = 0;
+        int counts = 0;
 
-        long long int prefix_sum = 0;
-        long long int counts = 0;
-
-        while(j < n)
+        unordered_map<int,int>mp;
+        for(int i = 0;i<n;i++)
         {
-            prefix_sum += nums[j];//1 2 2 3 //1 2 1 2 //0
-
-            while(prefix_sum > goal)
+            prefix_sum += nums[i];
+            if(prefix_sum == goal)
             {
-                prefix_sum -= nums[i];//1 2 2 3-=1 == 2 1 1//0
-                i++;//1 2 3 //1 2
+                counts++;
             }
 
-            counts += (j-i+1);//1 2 3 4 5    // 6 4 4 == 14 // 1 2 2 3 2
+            if(mp.find(prefix_sum-goal)!=mp.end())
+            {
+                counts +=mp[prefix_sum-goal];
+            }
 
-            j++;//1 2 3 4 5 // 1 2 3 4 5
+            mp[prefix_sum]++;
         }
 
         return counts;
 
-    }
-
-
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-
-        //Using sliding window , calculating the number of subarrays with sum <= k is possible.But exactly k is not possible.So, We will calculate the number of subarrays having sum exactly k as (no. of subarrays with sum <=k ) - (no. of subarrays with sum <= (k-1) ).
-
-        //This functions calculate the number of subarrays sum <= x.
-
-        int k = goal;
-        return subarrays(nums,k) - subarrays(nums,k-1);
-
+        //TC : O(n)
+        //SC : O(n)
+        
     }
 };
