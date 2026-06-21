be81a15b78247d826d19bf3d3ae0e50b71cd1f8d
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-21 13:09:32 +0530

    Time: 12 ms (90.00%) | Memory: 278 MB (60.00%) - LeetSync

--- 4330-minimum-lights-to-illuminate-a-road/minimum-lights-to-illuminate-a-road.cpp
+++ 4330-minimum-lights-to-illuminate-a-road/minimum-lights-to-illuminate-a-road.cpp
@@ -5,9 +5,7 @@ public:
 
         int n = lights.size();
 
-        vector<int>diff(n,0);//Stores whether the ith location is lighten or not.
-        //diff[i] +ve means that ith location is lighten.
-        //diff[i] -ve means that ith location is not lighten.
+        vector<int>diff(n,0);//Change in the number of active bulbs at location i.
 
         int l = 0;
         int r = 0;
@@ -18,6 +16,7 @@ public:
             {
                 l = max(0, i - lights[i]);//left most boundary till which light can reach. 
                 r = min(n - 1, i + lights[i]);//right most boundary till which light can reach.
+
                 diff[l]++;
 
                 if(r+1 < n)
@@ -32,27 +31,27 @@ public:
 
         //After Computing the difference array -> We take it's prefix sum.
 
-        int running_bulbs = 0;//stores prefixsum    
+        int running_bulbs = 0;//stores prefixsum  : Total Number of bulbs illuminating the ith location. 
 
         for(int i = 0 ; i< n;i++)
         {
-            running_bulbs += diff[i];
+            running_bulbs += diff[i];//0  
             if(running_bulbs == 0)
             {
-                //not lighten.we need to place one bulb.
-                min_bulbs++;
-                running_bulbs++;
-                //This bulb can enlighten from -1 to +1 pos.
+                //No active bulb to illumiate ith location.
+                min_bulbs++;//1
+                running_bulbs++;//Now there is one bulb to illuminate that ith location.
+                //This bulb can enlighten from -1 to +1 pos.//
 
-                int pos = min(n-1,i+1);// greedily we will place the bulb at the i+1 th location.so both i and i+2 are covered in one go.
+                int pos = min(n-1,i+1);// greedily we will place the bulb at the i+1 th location.so both i and i+2 are covered in one go.//place bulb at (i+1)th.
 
-                int l = max(pos-1,0);
-                int r = min(n-1,pos+1); 
+                //Now this placed bulb can may enlighten the other bulbs on it's left and right.So we need to specify those ranges of enlightening in the diff[i]
 
-                diff[l]++;
+                int l = max(pos-1,0);//ith location set 1
+                int r = min(n-1,pos+1);//i+2 th location 
 
                 if(r+1 < n)
-                diff[r+1]--;
+                diff[r+1]--;//it is (i+3)th one set --.
             }
         }
 
