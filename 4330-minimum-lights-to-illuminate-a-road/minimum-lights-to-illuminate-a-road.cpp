a5d4d28706a2f05d9dd08f5d18a9f35c556d08d3
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-21 13:31:00 +0530

    Time: 14 ms (90.00%) | Memory: 277.9 MB (60.00%) - LeetSync

--- 4330-minimum-lights-to-illuminate-a-road/minimum-lights-to-illuminate-a-road.cpp
+++ 4330-minimum-lights-to-illuminate-a-road/minimum-lights-to-illuminate-a-road.cpp
@@ -1,61 +1,59 @@
 class Solution {
 public:
-    
     int minLights(vector<int>& lights) {
 
         int n = lights.size();
 
-        vector<int>diff(n,0);//Change in the number of active bulbs at location i.
+        vector<int>diff(n,0); 
 
-        int l = 0;
-        int r = 0;
-
-        for(int i =0; i<n;i++)
+        for(int i = 0; i  < n;i++)
         {
             if(lights[i]!=0)
             {
-                l = max(0, i - lights[i]);//left most boundary till which light can reach. 
-                r = min(n - 1, i + lights[i]);//right most boundary till which light can reach.
+                //take the range upto which it illuminates light.
+                int v = lights[i];
+                int l = max(0, i - v) ;
+                int r = min(n - 1, i + v);
 
                 diff[l]++;
 
                 if(r+1 < n)
+                {
+                    diff[r+1]--;
+                }
+            }
+        }
+
+
+        int min_bulbs = 0;
+        
+        int prefix_sum = 0;//Gives the cumulative number of bulbs that lighten the ith location.
+
+        for(int i = 0 ; i < n;i++)
+        {
+            prefix_sum += diff[i];
+
+            if(prefix_sum == 0)
+            {   
+                //i.e No bulb is there which illuminates this ith location.
+                min_bulbs++;//we inserted a new bulb.
+                prefix_sum++;//One more bulb added.
+
+                //you need to Modify the righmost range upto which this newly inserted bulb will lighten.
+
+                //If there is no bulb lightening ith location , we will insert the new bulb at the (i+1)th location greedily .
+                
+                int pos = min(n-1,i+1);
+
+                int r = min(n-1,pos+1);//getting (i+2)th one pos ater bulb got inserted.light of new bulb can reach here.
+
+                //set -1 at (i+3)th location where the light of the newly inserted bulb at the (i+1)th location cant reach.
+                
+                if(r+1 < n)
                 diff[r+1]--;
             }
         }
-
-
-        //Now compute the Minimum number of bulbs needed to lighten every single location.
-
-        int min_bulbs = 0;
-
-        //After Computing the difference array -> We take it's prefix sum.
-
-        int running_bulbs = 0;//stores prefixsum  : Total Number of bulbs illuminating the ith location. 
-
-        for(int i = 0 ; i< n;i++)
-        {
-            running_bulbs += diff[i];//0  
-            if(running_bulbs == 0)
-            {
-                //No active bulb to illumiate ith location.
-                min_bulbs++;//1
-                running_bulbs++;//Now there is one bulb to illuminate that ith location.
-                //This bulb can enlighten from -1 to +1 pos.//
-
-                int pos = min(n-1,i+1);// greedily we will place the bulb at the i+1 th location.so both i and i+2 are covered in one go.//place bulb at (i+1)th.
-
-                //Now this placed bulb can may enlighten the other bulbs on it's left and right.So we need to specify those ranges of enlightening in the diff[i]
-
-                int l = max(pos-1,0);//ith location set 1
-                int r = min(n-1,pos+1);//i+2 th location 
-
-                if(r+1 < n)
-                diff[r+1]--;//it is (i+3)th one set --.
-            }
-        }
-
         return min_bulbs;
-
+        
     }
 };
