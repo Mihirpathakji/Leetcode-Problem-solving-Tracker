82810c34c336d62fa60024dd4ffdfd0b86f4dde9
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-22 21:49:26 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (63.27%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,51 +1,27 @@
 class Solution {
 public:
-
-    int index(vector<int>&nums,int target)
-    {
-        //Count number of elements which are >= target.
-       
-        int counts = 0;
-        for(int i = 0 ; i  < nums.size(); i++)
-        {
-            if(nums[i] >= target)
-            {
-                counts++;
-            }
-        }
-        return counts;
-    }
-
     int specialArray(vector<int>& nums) {
 
-        //[0,0,3,4,4].
+        int n = nums.size();
 
-        int low = 0;
-        int high = *max_element(nums.begin(),nums.end());
-
-        while(low <= high)
+        for(int x = 0;x <= 1000; x++)
         {
-            int mid = low + (high-low);//500
-            int values = index(nums,mid);//0
-            if(values == mid)
+            //calculate the no. of elements >= x in array.
+
+            int counts = 0;
+            for(int i = 0; i< n; i++)
             {
-                return values;
+                if(nums[i] >= x)
+                {
+                    counts++;
+                }
             }
-            else if(values > mid)
+            if(counts == x)
             {
-                //No.of values >= mid in array are even greater than the mid.i.e your mid is too smaller increment the mid -> increment the low.
-                low = mid +1;//
-            }
-            else
-            {
-                //No. of values >= mid are even lessser than the mid.i.e your mid is too large decrement the mid -> decrement the high.
-                high = mid -1;
+                return x;
             }
         }
         return -1;
-
-        //TC : O(n*logn)
-        //SC : O(1)
-
+        
     }
 };
