a73bbf622fed0793ec956e0166892ae26500a357
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-23 01:43:47 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (96.17%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,59 +1,70 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n=nums.size();
-        int low=0;
-        int high=n-1;
-        int mid;
-        //Approach :- Here the vector is rotated sorted .So you need to identify which Portion of the vector is sorted??? Thereby apply binary search only on that much sorted portion.i.e Eliminate the remaining search space.  
+            
+        int n = nums.size();
 
+        int low = 0;
+        int high = n-1;
 
-        while(low<=high)
+        while(low <= high)
         {
-            mid=low+(high-low)/2;
+            int mid = low + (high-low)/2;
 
-            if(nums[mid]==target)
+            if(nums[mid] == target)
             {
                 return mid;
             }
-            //1.If nums[mid]!=target->This is an Rotated sorted Array You need to check Which portion is sorted.Whether the Left portion is sorted or right?
-            //Is Left Portion sorted??
-            else if(nums[low]<=nums[mid])
+
+            //In rotated sorted array : 
+            //1.It is fixed that one half will be sorted and another will not be sorted from [low,mid] & [mid,high].
+
+            //We cannot blindly eliminate one search space completely.We need to identify that which half is sorted.thereby searching would be optimal.
+
+
+            //Identify which half is sorted.After jumping into mid index.
+
+            //1.First part.[low,mid].
+
+            if(nums[low] <= nums[mid])
             {
-                //Yes.
-                //Is my element lying on left sorted part?
-                if(nums[low]<=target && target<nums[mid])
+                //left part is sorted.Check if our x can exists in this range.
+
+                if(nums[low] <= target && target <= nums[mid])
                 {
-                    //Yes.
-                    //search only on that "left" part.
-                    high=mid-1;
+                    //x is on left half search on left half.
+                    high = mid -1;
                 }
                 else
                 {
-                    //Left portion sorted but element not on left part->Search on right part:
-                    low=mid+1;
+                    //left half is sorted.but our x is not in the left half.Search on right half.
+                    low = mid +1;
                 }
             }
+           
             else
             {
-                //Left portion is "not" sorted ->Right Portion "must be" sorted
-                //Okay then check if my target lying on right part or not!
-                if(nums[mid]<target && target<=nums[high])
+                //If the left part is not sorted.Than it is sure than in a rotated sorted array, if left part is not sorted than the right part will be sorted.
+
+                //Check if our target lies on right half.[mid,high].
+
+                if(nums[mid] <= target && target <= nums[high])
                 {
-                    //Yes.
-                    //Search only on right part ."Eliminate the Left search space"
-                    low=mid+1;
+                    //x is on right half .Searh on right half.
+
+                    low = mid + 1;                      
                 }
                 else
                 {
-                    //right part sorted but target not on right part .
-                    //i.e search on the left part only .Eliminate the right search space.
-                    high=mid-1;
+                    //x is not in right half.Search on left half.
+
+                    high = mid - 1;
                 }
+
             }
         }
-            return -1;
-            //TC:O(log2(n))
-            //SC:O(1)
+
+        return -1;
+
     }
 };
