2a9dd19c921371842fba39cbe3f188d1d4fc0b15
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-22 18:06:21 +0530

    Time: 0 ms (100.00%) | Memory: 69.7 MB (26.86%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -5,9 +5,9 @@ public:
         int n = nums.size();
         int i = 0;
         int j = 0;
-        int zeroes_count = 0;
+        int zeroes_count = 0;//If No. of zeroes are in the range of [0,k] than we can convert them IMAGINARILY INTO ONES.
         int max_len = INT_MIN;
-        
+
         while(j < n)
         {
             if(nums[j] == 0)
@@ -15,6 +15,10 @@ public:
                 zeroes_count++;
             }
 
+            //If No. of zeroes in our window are > k i.e All zeroes in our window cant be converted into ones.i.e the zeroes_count th zero cannot be converted into ones.We will remove leftmost zero from our window and ,we will use operation of flipping it in flipping the rightmost zeroes_count th zero.see if max_len can be improved or not.
+
+
+            //Invalid window shrink.
             while(zeroes_count > k)
             {
                 if(nums[i] == 0)
@@ -29,5 +33,7 @@ public:
         }
 
         return max_len;
+
+
     }
 };
