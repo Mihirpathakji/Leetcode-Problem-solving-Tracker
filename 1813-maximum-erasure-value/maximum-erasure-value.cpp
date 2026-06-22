2b970798e96897dc96792f9501c780106ed8d768
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-22 19:15:47 +0530

    Time: 135 ms (62.35%) | Memory: 117.9 MB (58.48%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/maximum-erasure-value.cpp
@@ -7,16 +7,20 @@ public:
         int j = 0;
         int curr_sum = 0;
         int max_sum= INT_MIN;
-
-
+  
         unordered_map<int,int>mp;
 
         while(j < n)
         {
-            curr_sum += nums[j];
-            mp[nums[j]]++;
+            curr_sum += nums[j];//always.
+            mp[nums[j]]++;//always.
 
-            while(mp.size()!=j-i+1)
+
+            //Invalid window : The Number of uniques elements in subarray are less than it's size i.e  All elements in the subarray are not unique there are duplicates in window.
+        
+           //Invalid window shrink.
+
+           while(mp.size() < j-i+1)
             {
                 curr_sum -= nums[i];
                 mp[nums[i]]--;
