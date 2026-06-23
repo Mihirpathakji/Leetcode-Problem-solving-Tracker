24524feebd88bf22f4c56a117bb3f54b7f77239c
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-23 16:30:05 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (44.48%) - LeetSync

--- 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.cpp
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.cpp
@@ -3,59 +3,64 @@ public:
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
 
         int n  = nums.size();
-        int n1 = queries.size();
+        int m = queries.size();
 
-        sort(nums.begin(),nums.end());
-        vector<int>prefix_sum(n);
-        prefix_sum[0] = nums[0];
+        sort(nums.begin(),nums.end());//n*logn
+        //since after sorting array we just need it's prefix sum, we can also do that in place.
 
         for(int i = 1; i < n; i++)
         {
-            prefix_sum[i] = prefix_sum[i-1] + nums[i]; 
+            nums[i] = (nums[i] + nums[i-1]);
         }
-
 
         //For every Query i will binary search on the array nums.
 
         vector<int>ans;
 
-        for(int i = 0; i  < n1; i++)
+        for(int i = 0; i  < m; i++)
         {
-            int target = queries[i];//10
+            int target = queries[i];//10 21 //1
 
             int low = 0;
-            int high = n-1;
+            int high = n-1;//index.3
            
             bool flag = false;
 
-            int possible_ans = 0; 
+            int possible_ans = 0;// If no elements in the array gave sum <= target.
          
             while(low <= high)
             {
-                int mid = low + (high-low)/2;//1 2
+                int mid = low + (high-low)/2;//1 2 3 //1 0
 
-                if(prefix_sum[mid] == target)
+                if(nums[mid] == target)
                 {
                     flag = true;
-                    ans.push_back(mid+1);//2
+                    ans.push_back(mid+1);//
                     break;
                 }
-                else if(prefix_sum[mid] < target)
+                else if(nums[mid] < target)
                 {
-                    low = mid +1;
-                    possible_ans = mid+1;
+                    low = mid +1;//3 // 
+                    possible_ans = mid+1;//2 3 4 //
                 }
                 else
                 {
                     high = mid -1;
                 }
-            }   
+
+            }//logn  
+           
             if(!flag)
             {
                 ans.push_back(possible_ans);
             }
+        
         }
 
         return ans;
+   
+        //TC : O(m*logn)
+        //SC : O(n)
+   
     }
 };
