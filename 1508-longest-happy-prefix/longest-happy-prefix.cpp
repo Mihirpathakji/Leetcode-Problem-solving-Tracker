0132324fb5534a7a9e2490a14ad083e48824cdb2
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-24 18:08:44 +0530

    Time: 12 ms (26.84%) | Memory: 24.5 MB (14.53%) - LeetSync

--- 1508-longest-happy-prefix/longest-happy-prefix.cpp
+++ 1508-longest-happy-prefix/longest-happy-prefix.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     string longestPrefix(string s) {
 
+        //KMP Algorithm : To find the longest Proper common prefix which is also a proper suffix.
+
         int n = s.length();
-        vector<int>lps(n,0);
+        vector<int>lps(n,0);//longest prefix suffix.
 
         int prefix = 0;
         int suffix = 1;
@@ -12,8 +14,8 @@ public:
         {
             if(s[prefix] == s[suffix])
             {
-                lps[suffix] = prefix + 1;//Maximum length of common proper prefix suffix till index suff.
-
+                lps[suffix] = prefix + 1;//Maximum length of common proper prefix and proper suffix for a string which ends at index suffix.
+                
                 prefix++;
                 suffix++;
             }
@@ -21,7 +23,7 @@ public:
             {
                 if(prefix == 0)
                 {
-                    lps[suffix] = 0;
+                    lps[suffix] = 0;//I cannot send the prefix to backwards further since it will become negative.thereby lps[-1] not accessible.
                     suffix++;
                 }
                 else
@@ -31,16 +33,20 @@ public:
             }
         }
 
-        int limit = lps[n-1];
+        int limit = lps[n-1];//Contains the index "before" which all elements forms the longest common proper prefix suffix for the given entire string.
+
         string ans;
 
         for(int i = 0; i < limit;  i++)
         {
             ans.push_back(s[i]);
         }
+        
         return ans;
         
         //TC : O(n)
         //SC : O(n)
+
+
     }
 };
