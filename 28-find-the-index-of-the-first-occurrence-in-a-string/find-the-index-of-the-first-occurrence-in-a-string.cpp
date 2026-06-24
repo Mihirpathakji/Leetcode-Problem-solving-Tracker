53c6e5bb60fd694908e71e7e2280d937e153dfce
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-24 22:56:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (16.15%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -2,14 +2,81 @@ class Solution {
 public:
     int strStr(string haystack, string needle) {
 
-        if(haystack.find(needle)!= string :: npos)
-        {
-            return haystack.find(needle);
-        }   
-        else
-        {
-            return -1;
-        }
+
+        int n = haystack.size();
+        int m = needle.size();
+
+        vector<int>lps(m,0);
         
+        //Compute the lps of needle:
+
+        int prefix = 0;
+        int suffix = 1;
+
+        while(suffix < m)
+        {
+            //Comparision :
+
+            if(needle[prefix] == needle[suffix])
+            {
+                lps[suffix] = prefix+1;//length of lcps ending at index suffix.
+                prefix++;
+                suffix++;
+            }
+            else
+            {
+                if(prefix == 0)
+                {
+                    lps[suffix] = 0;
+                    suffix++;
+                }
+                else
+                {
+                    prefix = lps[prefix-1];
+                }
+            }
+        }//O(m)
+
+        int first = 0;
+        int second = 0;
+
+        while(first < n)
+        {
+            if(second == m)
+            {
+                return first-second;               
+            }
+            
+            if(haystack[first] == needle[second])
+            {
+                first++;
+                second++;
+            }
+            else
+            {
+                if(second == 0)
+                {
+                    first++;
+                }
+                else
+                {
+                    second = lps[second-1];
+                }
+            }
+
+        }//O(n)
+
+        if(first ==n && second==m)
+        {
+            return first-second;
+        }
+
+        return -1;
+
+        //TC : O(n+m)
+        //SC : O(m)
+
+
+
     }
 };
