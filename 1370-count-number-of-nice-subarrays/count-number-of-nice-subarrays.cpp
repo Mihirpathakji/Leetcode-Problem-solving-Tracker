c76905152103a2e4bb4ced411a0534238b4f2390
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-25 14:08:12 +0530

    Time: 51 ms (21.10%) | Memory: 94.1 MB (20.21%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -4,49 +4,39 @@ public:
 
         int n = nums.size();
 
-        //Replace all odd values with 1 and all even values with 0.
-
         unordered_map<int,int>mp;//To store the Oddcount for the odd numbers seen in past.
 
-        for(int i = 0;i  < n;i++)
-        {
-            if(nums[i]%2 == 1)
-            {
-                nums[i] = 1;
-            }
-            else
-            {
-                nums[i] = 0;
-            }
-        } 
-
-        //Now ans is subarray sum equals k.
-
-        int prefix_sum = 0;
+        int prefix_Odd_count = 0;
         int counts = 0;
 
         for(int i = 0;i < n;i++)
-        {
-            prefix_sum += nums[i];
+        {   
+            if(nums[i] % 2 == 1) {
+                prefix_Odd_count ++;
+            }
 
             //Edge case :
 
             //If any prefix subarray has oddcount exactly k.
 
-            if(prefix_sum == k)
+            if(prefix_Odd_count == k)
             {
                 counts++;
             }
 
-            if(mp.find(prefix_sum-k)!=mp.end())
+            if(mp.find(prefix_Odd_count-k)!=mp.end())
             {
-                counts += mp[prefix_sum-k];
+                counts += mp[prefix_Odd_count-k];
             }
 
-            mp[prefix_sum]++;
+            mp[prefix_Odd_count]++;
+            
         }
 
         return counts;
+
+        //TC : O(n)
+        //SC : O(n)
 
         
     }
