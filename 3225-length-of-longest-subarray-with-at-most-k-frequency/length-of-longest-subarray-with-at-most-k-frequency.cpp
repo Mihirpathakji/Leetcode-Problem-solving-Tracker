571bb74bd91033222bb498f845efac6cb674d4bb
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-25 23:51:56 +0530

    Time: 79 ms (19.00%) | Memory: 151.2 MB (5.86%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -15,30 +15,24 @@ public:
         while(j  < n)
         {
         
-            mp[nums[j]]++;//2 
-            max_freq = max(max_freq,mp[nums[j]]);//1 2 2 2 
+            mp[nums[j]]++;//1 3
 
-            while(max_freq > k)
+            while(mp[nums[j]] > k)
             {
-                
-                if(mp[nums[i]] == max_freq)
-                {
-                    max_freq--;//1.But may possible that there exists one more element with the same max_freq.Than we didnt remove it.But we have to remove it.//1 1
-                }
+                mp[nums[i]]--;
 
-                mp[nums[i]]--;//1 
-              
                 if(mp[nums[i]] == 0)
                 {
                     mp.erase(nums[i]);
-                }    
-                
+                }
+
                 i++;
             }
 
-            max_len = max(max_len,j-i+1);// 1 2 2 2
+            max_len = max(max_len,j-i+1);
 
-            j++;// 1 2 3 4 
+            j++;
+
         }
 
         return max_len;
