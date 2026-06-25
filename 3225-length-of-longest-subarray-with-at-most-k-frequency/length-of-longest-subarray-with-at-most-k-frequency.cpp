85aeb2ec791abcf1d429f48d69304701c75fc850
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-25 22:48:35 +0530

    Time: 77 ms (20.74%) | Memory: 151.1 MB (9.11%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -9,34 +9,38 @@ public:
         int max_freq = INT_MIN;//What is the Maximum frequency of any element in my subarray.
 
         unordered_map<int,int>mp;
+
         int max_len = INT_MIN;
 
         while(j  < n)
         {
-            mp[nums[j]]++;//1 1 2
-            max_freq = max(max_freq,mp[nums[j]]);//1 1 2
+        
+            mp[nums[j]]++;//2 
+            max_freq = max(max_freq,mp[nums[j]]);//1 2 2 2 
 
-            while(i <= j && max_freq > k)
+            while(max_freq > k)
             {
                 
                 if(mp[nums[i]] == max_freq)
                 {
-                    max_freq--;//1.But may possible that there exists one more element with the same max_freq.Than we didnt remove it.But we have to remove it.//
+                    max_freq--;//1.But may possible that there exists one more element with the same max_freq.Than we didnt remove it.But we have to remove it.//1 1
                 }
 
-                mp[nums[i]]--;
+                mp[nums[i]]--;//1 
+              
                 if(mp[nums[i]] == 0)
                 {
                     mp.erase(nums[i]);
                 }    
                 
-                i++;//
+                i++;
             }
 
-            max_len = max(max_len,j-i+1);//1 2
+            max_len = max(max_len,j-i+1);// 1 2 2 2
 
-            j++;
+            j++;// 1 2 3 4 
         }
+
         return max_len;
 
     }
