a1a7794a1c8dd9f729c32556c13fb9971e306e54
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-26 00:12:25 +0530

    Time: 69 ms (36.66%) | Memory: 151 MB (12.59%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -6,6 +6,11 @@ public:
         int i = 0;
         int j = 0;
 
+        int max_freq = INT_MIN;//What is the Maximum frequency of any element in my subarray.
+
+        //There will Never exists more than 2 element with same MAX FREQUNECY > K.BCZ, Movement i find a invalid window with > k i removed elements and thereby every window will have only ONE ELEMENT AT WORST WITH FREQ > K
+
+
         unordered_map<int,int>mp;
 
         int max_len = INT_MIN;
@@ -14,17 +19,29 @@ public:
         {
         
             mp[nums[j]]++;
+            max_freq = max(max_freq,mp[nums[j]]);
 
-            while(mp[nums[j]] > k)
+            while(max_freq > k)
             {
+                
+                if(mp[nums[i]] == max_freq)
+                {
+                    max_freq--;
+                }
+
                 mp[nums[i]]--;
+              
+                if(mp[nums[i]] == 0)
+                {
+                    mp.erase(nums[i]);
+                }    
+                
                 i++;
             }
 
             max_len = max(max_len,j-i+1);
 
             j++;
-
         }
 
         return max_len;
