6712b9c118370a55c1072540bfa2adb0522be334
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-26 19:38:59 +0530

    Time: 69 ms (56.95%) | Memory: 114.9 MB (48.34%) - LeetSync

--- 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.cpp
+++ 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.cpp
@@ -7,21 +7,23 @@ public:
         unordered_map<int,int>mp;
 
         mp[0] = 1;
+
         long long prefix_sum = 0;
+        
         long long subarray_counts = 0;
-        long long valid_left_points = 0;
+        long long valid_left_points = 0;//varaible to track how many indices are there before index j which satisfies pf[i-1] < pf[j].
 
         for(int j = 0;  j < n; j++)
         {
-
+            
             if(nums[j] == target)
             {
                 valid_left_points += mp[prefix_sum];
-                prefix_sum ++;
+                prefix_sum += 1;
             }
             else
             {
-                prefix_sum--;
+                prefix_sum += -1;        
                 valid_left_points -= mp[prefix_sum];
             }
 
