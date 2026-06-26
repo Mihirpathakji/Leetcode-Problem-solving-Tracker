9252de8dd50ba2193ec2661193576e098ae5c443
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-26 12:27:11 +0530

    Time: 30 ms (83.55%) | Memory: 39.5 MB (96.10%) - LeetSync

--- 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
+++ 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
@@ -5,25 +5,40 @@ public:
         //Brute Force :
 
         int n = nums.size();  
-        int subarray_counts = 0;
+        int subarray_counts = 0;      
+        
+        for(int i = 0;i < n;i++) {
+
+            if(nums[i] == target) {
+                nums[i] = 1;    
+            }
+            else {
+                nums[i] = -1; 
+            }
+        }   
+
+        for(int i = 1;i < n;i++)
+        {
+            nums[i] = (nums[i-1]+nums[i]);
+        }
 
         for(int i = 0;i < n;i++)
         {
-            int counts_target = 0;
-
             for(int j = i;j < n;j++)
             {
-                if(nums[j] == target) {
-                    counts_target += 1;
-                }
-                else {
-                    counts_target -=1;
+                int sum;
+               
+                if(i > 0) { 
+                    sum = nums[j] -nums[i-1];
                 }
 
-                if(counts_target > 0) {
+                else {
+                    sum = nums[j];
+                }
+
+                if(sum >0) {
                     subarray_counts++;
                 }
-
             }
         }
         
