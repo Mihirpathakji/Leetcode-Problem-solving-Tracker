e2cb80231fb87216410eaa72a9d6bd5032f9e5f0
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-27 15:45:38 +0530

    Time: 7 ms (25.76%) | Memory: 27.2 MB (86.33%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -23,7 +23,7 @@ public:
         }
 
         return -1;
-    }
+    }//O(n)
 
     TreeNode* Tree(vector<int> &inorder,vector<int> &postorder,int instart,int inend,int postindex)
     {   
@@ -34,26 +34,30 @@ public:
 
         //1.Create a node with value of the postorder[postindex].
 
-        TreeNode* root = new TreeNode(postorder[postindex]);
+        TreeNode* root = new TreeNode(postorder[postindex]);//3 9 20 15 7 // -1
 
         //2.We didnt know the direction either left or right where to insert this node.Hence ,find the index of the postorder[postindex] value in the inorder.
 
-        int pos = find(inorder,instart,inend,postorder[postindex]);
+        int pos = find(inorder,instart,inend,postorder[postindex]);//1 0 3 2 4 // O(n)
 
         //3.Build left subtree and the rightsubtree.
 
-        root->left = Tree(inorder,postorder,instart,pos-1,postindex-(inend-pos)-1);
+        root->left = Tree(inorder,postorder,instart,pos-1,(postindex-(inend-pos))-1);//N
 
-        root->right = Tree(inorder,postorder,pos+1,inend,postindex-1);
+        root->right = Tree(inorder,postorder,pos+1,inend,postindex-1);//N
 
         return root;
 
-    }
+    }//O(n^2)
 
 
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
 
         return Tree(inorder,postorder,0,inorder.size()-1,postorder.size()-1);
-        
+        //root element in the postorder will be at postorder.size()-1.     
+
+        //TC : O(n^2)
+        //SC : O(n)
+
     }
 };
