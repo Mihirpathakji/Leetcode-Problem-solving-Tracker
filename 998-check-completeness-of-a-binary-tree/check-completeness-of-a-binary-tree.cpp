19730f4fabfa4e8bd8c75dd83c4125147baf5dd4
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-28 21:13:45 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (81.04%) - LeetSync

--- 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.cpp
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.cpp
@@ -11,110 +11,41 @@
  */
 class Solution {
 public:
+
     bool isCompleteTree(TreeNode* root) {
 
-        int levels = 0;
-
         queue<TreeNode*>q;
-
         q.push(root);
+        bool past_null = false;
 
-        while(!q.empty())
-        {
+        while(!q.empty()) {
+
             int n = q.size();
 
-            while(n--)
-            {
+            while(n--) {
+
                 TreeNode* temp = q.front();
                 q.pop();
 
-                if(temp->left) {
-                    q.push(temp->left);
-                }       
-
-                if(temp->right) {
-                    q.push(temp->right);
-                }  
-            }
-            levels++;
-        } 
-
-        int last_level = levels-1;
-
-        int curr_level = 0;
-
-        vector<int>ans;
-        q.push(root);
-        while(!q.empty())
-        {
-            int n = q.size();
-            int counts = 0;
-            while(n--)
-            {
-                TreeNode* temp = q.front();
-                q.pop(); 
-                
-                if(curr_level == last_level && temp!=NULL) {
-                    ans.push_back(temp->val);
+                if(temp == NULL) {
+                    past_null = true;
+                    continue;
                 }
-
-                if(temp) {
-                    counts++;
-                } 
-               
-                if(temp && temp->left) {
-                    q.push(temp->left);
-                }       
-                else if(temp && !temp->left && curr_level == last_level-1) {
-                    q.push(NULL);
-                }
-      
-                if(temp && temp->right) {
-                    q.push(temp->right);
-                }  
-                
-                else if(temp && !temp->right && curr_level == last_level -1) {
-                    q.push(NULL);
-                }
-
-                if(temp == NULL && curr_level == last_level) {
-                    ans.push_back(-1);
-                }
-            }
-
-            if(counts!=pow(2,curr_level) && curr_level!=last_level) {
-                return false;
-            }
-
-            curr_level++;
-
-            if(curr_level-1 == last_level) {
-
-                int n1 =  ans.size();
-                int index = -1;
-                bool flag = false;
-
-                for(int i = 0;i < n1;i ++)
-                {
-                    if(ans[i] == -1) {
-                        index = i;
-                        flag = true;
-                        break;
+                else {
+                    if(past_null) {
+                        return false;
                     }
                 }
 
-                for(int i = index+1;i< ans.size();i++)
-                {
-                    if(ans[i]!=-1 && flag)
-                    return false;
-                }
-            }   
+                q.push(temp->left);
+                q.push(temp->right);
+            }
         }
 
         return true;
 
         //TC : O(n)
-        //SC : O(n)
+        //SC : O(1)
         
     }
 };
