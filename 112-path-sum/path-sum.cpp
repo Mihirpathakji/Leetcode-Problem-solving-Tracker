cc39f61147359a37b147ea010d72a6da573721b2
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-30 16:55:12 +0530

    Time: 0 ms (100.00%) | Memory: 21.3 MB (96.05%) - LeetSync

---
+++ 112-path-sum/path-sum.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    void mydfscheck(TreeNode* root,int targetSum,int& sum, bool& flag) {
+
+        if(root == NULL) {
+            return;
+        }
+
+        sum += root->val;
+
+        if(root->left == NULL && root->right == NULL && sum == targetSum) {
+            flag = true;
+        }
+
+        mydfscheck(root->left,targetSum,sum,flag);
+        mydfscheck(root->right,targetSum,sum,flag);
+
+        //we had done checking this path.
+
+        sum -= root->val;
+
+    }
+
+    bool hasPathSum(TreeNode* root, int targetSum) {
+
+        int sum = 0;
+        bool flag = false;
+        mydfscheck(root,targetSum,sum,flag);
+
+        return flag;
+
+    }
+};
