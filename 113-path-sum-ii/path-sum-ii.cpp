c32b861b79fe5cc1fe05924767764cad34e36758
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-30 15:33:46 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (99.40%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.cpp
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -16,35 +16,32 @@ public:
 
     void mydfssum(TreeNode* root,int& targetSum,int& sum,vector<int>& temp,vector<vector<int>>& ans) {
 
-        //if i reached leaf node check if sum has become targetSum or not.
-
         if(root == NULL) {
-            return;
+            return; 
         }
         
         if(root) {
-            sum += root->val;//9 20 22
-            temp.push_back(root->val);//[5,4,11,7] 
+            sum += root->val;//5 9 20 27
+            temp.push_back(root->val);//5 4 9 7 
         }
 
         if(!root->left && !root->right && sum == targetSum) {
             ans.push_back(temp);
         }
 
-        mydfssum(root->left,targetSum,sum,temp,ans);//N
-        mydfssum(root->right,targetSum,sum,temp,ans);//N  
+        mydfssum(root->left,targetSum,sum,temp,ans);
+        mydfssum(root->right,targetSum,sum,temp,ans);
 
         sum -= root->val;
         temp.pop_back();
 
-        return;
     }
 
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
 
         int sum = 0;
         vector<vector<int>>ans;
-        vector<int>temp;
+        vector<int>temp; 
 
         mydfssum(root,targetSum,sum,temp,ans);
 
