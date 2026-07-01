a7481a98f8d4b0aaf5f63fa3cc73ec887b2a6195
Mihirpathakji <mihirgithub123456@gmail.com>
2026-07-01 15:58:57 +0530

    Time: 111 ms (5.03%) | Memory: 98.5 MB (16.87%) - LeetSync

---
+++ 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+
+    bool possible_to_done(long long min_time,vector<int>& time,int totalTrips) {
+
+        long double trips_done = 0;    
+
+        for(int i = 0;i < time.size();i++) {
+            trips_done += ((long long )min_time/time[i]);
+        }
+
+        return trips_done>=totalTrips;
+
+    }
+
+    long long minimumTime(vector<int>& time, int totalTrips) {
+
+        int n = time.size();
+
+        long long low = 1;
+        long long high = 1e17;
+        long long ans = 1;
+
+        while(low <= high) {
+
+            long long mid = low + (high-low)/2;
+
+            if(possible_to_done(mid,time,totalTrips)) {
+                //store it and look for the minimum.
+                ans = mid;
+                high = mid -1;
+            }
+            else {
+                low = mid + 1;  
+            }
+        }
+
+        return ans;
+
+        //TC : O(n*logn)
+        //SC : O(1)
+
+    }
+};
