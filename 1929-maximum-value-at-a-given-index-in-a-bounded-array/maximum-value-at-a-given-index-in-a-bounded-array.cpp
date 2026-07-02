00786fa2c6ec20316b3af92d06cf46a66d041188
Mihirpathakji <mihirgithub123456@gmail.com>
2026-07-02 18:39:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (70.61%) - LeetSync

--- 1929-maximum-value-at-a-given-index-in-a-bounded-array/maximum-value-at-a-given-index-in-a-bounded-array.cpp
+++ 1929-maximum-value-at-a-given-index-in-a-bounded-array/maximum-value-at-a-given-index-in-a-bounded-array.cpp
@@ -1,61 +1,81 @@
 class Solution {
 public:
 
-    bool is_possible(long long target,int n,int index,int maxSum) {
+    bool is_possible(long long maxvalue,int n,int index,int maxSum) {
 
-        long long curr_sum = 0;
-        curr_sum += target;
+        //We need to check if the sum with this maxvalue = nums[index] is <= maxSum given or not.
+
+        long long sumobtained = maxvalue;
 
         long long left_sum = 0;
-        long long extraOnes_left = max(0ll,index-(target-1));
-        if(target-1 > index) {
-            left_sum += ((target-1)*(target)/2 -((target-1-index)*(target-1-index+1)/2));
-        }
-        else {
-            left_sum += (target-1)*(target)/2 + max(0ll,index-(target-1));
-        }
-
         long long right_sum = 0;
-        long long extraOnes_right = max(0ll,n-index-1-(target-1));
-        long long n2 = min(target-1,(long long)n-index-1);
 
-        if(target-1 > n-index-1) {
-            right_sum += ((target-1)*(target)/2 -((target-1-(n-index-1))*(target-1-(n-index-1)+1)/2));
-        }
+        if(maxvalue-1 > index) {
+
+            long long n1 = (maxvalue-1)*(maxvalue-1+1)/2;//sum from 1 to maxvalue-1.
+
+            long long n2 = (maxvalue-1-index)*(maxvalue-1-index+1)/2;//sum from 1 to maxvalue-1-index.
+
+            left_sum += (n1 - n2);
+        } 
         else {
-            right_sum = ( (target-1)*(target)/2 + extraOnes_right); 
-        }
 
-        curr_sum += left_sum;
-        curr_sum += right_sum;
+            //maxvalue-1 <=index
 
-        return curr_sum <= maxSum;
+            long long n1 = (maxvalue-1)*(maxvalue-1+1)/2;
+            long long extra_ones = max(0ll,(index-(maxvalue-1)));
+
+            left_sum += (n1 + extra_ones);
+        } 
+
+        if(maxvalue-1 > n-index-1) {
+
+            long long n1 = (maxvalue-1)*(maxvalue-1+1)/2;//sum from 1 to maxvalue-1.
+
+            long long n2 = (maxvalue-1-(n-index-1))*(maxvalue-1-(n-index-1)+1)/2;//sum from 1 to maxvalue-1-(n-index-1).
+
+            right_sum += (n1 - n2);
+        } 
+        else {
+
+            //maxvalue-1 <=index
+
+            long long n1 = (maxvalue-1)*(maxvalue-1+1)/2;
+            long long extra_ones = max(0ll,(n-index-1-(maxvalue-1)));
+
+            right_sum += (n1 + extra_ones);
+        } 
+
+        sumobtained += left_sum;
+        sumobtained += right_sum;
+
+
+        return sumobtained <= maxSum;
 
     }
 
     int maxValue(int n, int index, int maxSum) {
 
-        //Brute force :
-
-        long long target = maxSum;
-        long long max_ans = LLONG_MIN;
+        //Binary search on possbile maxSum values :
 
         long long low = 1;
         long long high = maxSum;
+        long long ans = 0;
 
-        while (low <= high) {
-        
-            long long mid = low + (high-low)/2;
+        while(low <= high) {
+
+            long long mid = low + (high-low)/2;//Expected max value of nums[index].
+
             if(is_possible(mid,n,index,maxSum)) {
-                max_ans = mid;
-                low = mid +1;
+                ans = mid;//
+                low = mid + 1;                
             }
             else {
-                high = mid-1;
+                high = mid -1;
             }
         }
 
-        return max_ans;
+        return ans;
 
         //TC : O(logmaxSum)
         //SC : O(1)
