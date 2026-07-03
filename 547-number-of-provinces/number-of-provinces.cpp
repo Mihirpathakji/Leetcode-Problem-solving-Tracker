bd589db70f4715fda4d77df6fc5451029b55c891
Mihirpathakji <mihirgithub123456@gmail.com>
2026-07-03 16:56:52 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (92.71%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    void mydfs(int u,vector<bool>& visited,vector<vector<int>>& adjacency_list) {
+
+        for(int i = 0;i < adjacency_list[u].size();i++) {
+            
+            if(adjacency_list[u][i] == 1) {
+                if(!visited[i]) {
+                    visited[i] = true;
+                    mydfs(i,visited,adjacency_list);
+                }
+            }
+        }
+
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+
+        int v = isConnected.size();
+   
+        vector<bool>visited(v,false);
+        int provinces = 0;
+
+        for(int i = 0;i < v;i++) {
+            
+            if(visited[i] == false) {
+                provinces++;
+                visited[i] = true;
+                mydfs(i,visited,isConnected);
+            }
+        }
+
+        return provinces;
+
+        //TC: O(v + 2*e)
+        //SC : O(v)
+
+    }
+};
