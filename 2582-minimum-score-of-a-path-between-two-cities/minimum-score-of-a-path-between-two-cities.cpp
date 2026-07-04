8117a6a2631579969b437ced2923072a0c657f2a
Mihirpathakji <mihirgithub123456@gmail.com>
2026-07-04 12:43:49 +0530

    Time: 83 ms (48.62%) | Memory: 138.1 MB (43.69%) - LeetSync

---
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+
+    void bfs(int& start,int& min_dist,queue<int>& q,vector<bool>&visited,unordered_map<int,vector<pair<int,int>>>& adj_list ) {
+
+        visited[start] = true;
+        q.push(start);
+
+        while(!q.empty()) {
+
+            int u = q.front();//1
+            q.pop();
+
+            //Traverse in the Neighbours of the start.
+
+            for(auto &v : adj_list[u]) {
+
+                if(visited[v.first] == false) {
+                    visited[v.first] = true;
+                    q.push(v.first);
+                }
+
+                min_dist = min(min_dist,v.second);  
+            }
+
+        }
+
+    }
+
+
+    int minScore(int n, vector<vector<int>>& roads) {
+        
+        int m = roads.size();
+        unordered_map<int,vector<pair<int,int>>>adj_list(m+1);//1-based.
+
+        for(int i = 0;i < m;i++) {
+            
+            int u = roads[i][0];
+            int v = roads[i][1];
+            int weight = roads[i][2];
+
+            adj_list[u].push_back({v,weight});  
+            
+            adj_list[v].push_back({u,weight});
+        }
+
+        vector<bool>visited(n+1,false);
+        queue<int>q;
+
+        int start = 1;//bfs only for 1 as start.
+
+        int min_dist = INT_MAX;
+
+        bfs(start,min_dist,q,visited,adj_list);
+
+        return min_dist;
+    }
+};
