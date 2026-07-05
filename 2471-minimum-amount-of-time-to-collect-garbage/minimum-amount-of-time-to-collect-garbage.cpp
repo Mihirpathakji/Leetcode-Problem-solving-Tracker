172f0ac2219ae7d8b3c2b1b871478971945b18ee
Mihirpathakji <mihirgithub123456@gmail.com>
2026-07-05 20:16:04 +0530

    Time: 50 ms (8.18%) | Memory: 107 MB (89.22%) - LeetSync

--- 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.cpp
+++ 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.cpp
@@ -1,127 +1,91 @@
 class Solution {
 public:
     int garbageCollection(vector<string>& garbage, vector<int>& travel) {
-        
-        int metal_time = 0;
-        int paper_time = 0;
-        int glass_time = 0;
 
-        set<int>glass;
-        set<int>paper;
-        set<int>metal;
+        int last_metal = -1;
+        int last_paper = -1;
+        int last_glass = -1;
 
         int n = garbage.size();
 
         for(int i = 0;i < n;i++) {
-           
-            string temp = garbage[i];
 
-            if(temp.find('P')!=string ::npos) {
-                paper.insert(i);
+            if(garbage[i].find('P')!=string::npos) {
+                last_paper = i;//1 2
             }
             
-            if(temp.find('G')!=string ::npos) {
-                glass.insert(i);
+            if(garbage[i].find('G')!=string::npos) {
+                last_glass = i;//0 2 3
             }
             
-            if(temp.find('M')!=string ::npos) {
-                metal.insert(i);
+            if(garbage[i].find('M')!=string::npos) {
+                last_metal = i;//-1
             }
-            
         }
 
-        long long min_time = 0;
+        int min_time = 0;
 
-        if(glass.size() >= 1) {
-
-            auto it =  glass.end();
-            it--;
-            int last = *it;//3
-
-            for(int i = 0;i <= last;i++) {
+        for(int i = 0;i <= last_paper;i++){
+            if(garbage[i].find('P')!=string::npos) {
                 
-                if(glass.find(i)!=glass.end()) {
-                    
-                    for(int j = 0;j <garbage[i].size() ;j++) {
-                        if(garbage[i][j] == 'G') {
-                            min_time++;//1 4 10 
-                        }
-                    } 
-
-                    if(i!=0) {
-                        min_time += travel[i-1];//8 13
-                    }
+                for(int j = 0;j < garbage[i].size();j++) {
+                    if(garbage[i][j] == 'P')
+                    min_time++;
                 }
-                else {
 
-                    if(i!=0) {
-                        min_time += travel[i-1];//+=2 // 3 
-                    }
-                }
-                
-            }        
-        }
-
-        
-        if(paper.size() >= 1) {
-            
-            auto it = paper.end();
-            it--;
-            int last = *it;//2
-            
-            for(int i = 0;i <= last;i++) {
-                
-                if(paper.find(i)!=paper.end()) {
-                    for(int j = 0;j <garbage[i].size() ;j++) {
-                        if(garbage[i][j] == 'P') {
-                            min_time++;//14 17
-                        }
-                    } 
-
-                    if(i!=0) {
-                        min_time += travel[i-1];//16  21
-                    }
-                }
-                else {
-
-                    if(i!=0) {
-                        min_time += travel[i-1];
-                    }
-                }
-            }
-            
-        } 
-
-        
-        if(metal.size()>=1) {
-
-            auto it =  metal.end();
-            it--;
-            int last = *it;
-
-            for(int i = 0;i <= last ;i++) {
-                
-                if(metal.find(i)!=metal.end()) {
-                    for(int j = 0;j <garbage[i].size() ;j++) {
-                        if(garbage[i][j] == 'M') {
-                            min_time++;
-                        }
-                    } 
-
-                    if(i!=0) {
-                        min_time += travel[i-1];
-                    }
-                }
-                else if(i!=0) {
+                if(i!=0) {
                     min_time += travel[i-1];
                 }
-            } 
+
+            }
+            else if(i!=0){
+                min_time += travel[i-1];
+            }
+        }
+
+        for(int i = 0;i <= last_glass;i++){
+
+             if(garbage[i].find('G')!=string::npos) {
+                
+                for(int j = 0;j < garbage[i].size();j++) {
+                    if(garbage[i][j] == 'G')
+                    min_time++;
+                }
+
+                if(i!=0) {
+                    min_time += travel[i-1];
+                }
+
+            }
+            else if(i!=0){
+                min_time += travel[i-1];
+            }
+        }
+
+        for(int i = 0;i <= last_metal;i++){
+
+             if(garbage[i].find('M')!=string::npos) {
+                
+                for(int j = 0;j < garbage[i].size();j++) {
+                    if(garbage[i][j] == 'M')
+                    min_time++;
+                }
+
+                if(i!=0) {
+                    min_time += travel[i-1];
+                }
+
+            }
+            else if(i!=0){
+                min_time += travel[i-1];
+            }
+        
         }
 
         return min_time;
 
-        //TC : O(n*logn)
-        //SC : O(n)
-
+        //TC : O(n)
+        //SC : O(1)
+        
     }
 };
