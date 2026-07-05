5c08297892e2b8ea07cf234aac732a793e7d53a3
Mihirpathakji <mihirgithub123456@gmail.com>
2026-07-05 16:20:55 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (70.02%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -2,71 +2,71 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
 
-        //{{r,c},t}
-
-        queue<pair<pair<int,int>,int>>q;//We will store the address of all the rotten oranges and the time where each rotten orange started it's bfs.
-
-
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>>visited(m,vector<int>(n,0));
 
-        //When we do BFS, Than at end the maximum time taken by any rottened is the ans.
+        //{{r,c},t}
+        queue<pair<pair<int,int>,int>>q;//To track the time where the each fresh element was infected.
 
-        for(int i = 0;i< m; i++) {
-            for(int j = 0;j < n;j++) {
-
-                if(grid[i][j] == 2) {
-                    q.push({{i,j},0});
-                    visited[i][j] = 2;
-                }
-            }
-        }
-        int mxtime = 0;
-
-        //Applying BFS to cover all fresh oranges.
-
-        //{row-1,col},{row+1,col},{row,col-1},{row,col+1}
-        
-        int delrow[4] = {-1,1,0,0};
-        int delcol[4] = {0,0,-1,1};
-
-        while(!q.empty()) {
-
-            int row = q.front().first.first;//1 
-            int col = q.front().first.second;//0 
-            int t = q.front().second;//0 0 1  //This {row,col} it tooked t time to get infected.
-            mxtime = max(t,mxtime);//1 1 1 2//Maxtime taken by any fresh element to get infected.
-
-            q.pop();
-
-            //Check all it's Neighbours.
-
-            for(int i = 0;i <4;i++) { 
-                if(row + delrow[i] >=0 && row + delrow[i] < m && col + delcol[i] >=0  && col + delcol[i] < n && grid[row+delrow[i]][col+delcol[i]] == 1 && visited[row+delrow[i]][col+delcol[i]] !=2) {
-
-                    visited[row+delrow[i]][col+delcol[i]] = 2;//Made it rotten.
-                    q.push({{row+delrow[i],col+delcol[i]},t+1});
-                }
-            }
-
-        }
-
-        //If any fresh orange is remaining to be rottened.
+        //1.Initializing queue with all the rottens since they were rotten originally their rottening time is 0.
 
         for(int i = 0;i < m;i++) {
             for(int j = 0;j < n;j++) {
-                if(grid[i][j] == 1 && visited[i][j]!=2) 
-                {   
+                
+                //pushing elements which were rotten initially.
+
+                if(grid[i][j] == 2) {
+                    q.push({{i,j},0});
+                }
+            }
+        }
+
+        //1.Doing bfs for all the rottens. 
+
+        int max_time_at_which_any_one_infected = 0;
+
+        //[row-1,col],[row,col-1],[row+1,col],[row,col+1].
+
+        while(!q.empty()) {
+
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+
+            int t = q.front().second;//cumulative time.
+            q.pop();
+
+            max_time_at_which_any_one_infected  = max(t,max_time_at_which_any_one_infected);
+
+            int delrow[4] = {-1,0,1,0};
+            int delcol[4] = {0,-1,0,1};
+
+            for(int i = 0;i < 4;i++) {
+
+                //if any fresh is in the neighbour than we need to convert it into the rotten.
+                if(row + delrow[i] >=0 && row + delrow[i] < m && col + delcol[i] >=0 && col + delcol[i] < n && grid[row + delrow[i]][col + delcol[i]] == 1 ) {
+                    
+                    grid[row + delrow[i]][col + delcol[i]] = 2;
+                    q.push({{row + delrow[i],col+delcol[i]},t+1});
+                }
+            }   
+
+        }
+
+        //If still any fresh orange remained in the grid after converting all possible fresh oranges into rotten one through bfs i.e not possible to cover that freshone.
+
+        for(int i = 0;i < m;i++) {
+            for(int j = 0;j < n;j++) {
+                if(grid[i][j] == 1) {
                     return -1;
                 }
             }
         }
 
-        return mxtime;
+        return max_time_at_which_any_one_infected;
 
-        //TC : O(m*n) + O(m*n*4)
+        //TC : O(m*n*4)
         //SC : O(m*n)
-        
+
+
     }
 };
