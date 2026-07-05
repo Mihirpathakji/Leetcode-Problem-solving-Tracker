240a20713760a675f5266340caeefac3f9ad025f
Mihirpathakji <mihirgithub123456@gmail.com>
2026-07-06 00:46:20 +0530

    Time: 15 ms (83.33%) | Memory: 30.4 MB (50.00%) - LeetSync

--- 4334-minimum-operations-to-transform-binary-string/minimum-operations-to-transform-binary-string.cpp
+++ 4334-minimum-operations-to-transform-binary-string/minimum-operations-to-transform-binary-string.cpp
@@ -9,7 +9,7 @@ public:
 
         int min_ops = 0;
 
-        int n = s1.length();
+        int n = s1.length();    
 
         for(int i = 0;i < n;i++) {
 
@@ -18,15 +18,28 @@ public:
             }   
             else if(s1[i] == '0' && s2[i] == '1') {
                 min_ops++;
+                s1[i] = '1';
             }
             else if(s1[i] == '1' && s2[i] == '0') {
-                if(s1[i+1] == '1') {
-                    min_ops++;
+                if(i+1!=n && s1[i+1] == '1') {
+                    min_ops++;//1
                     s1[i+1] = '0';
                 }
+                else if(i+1!=n && s1[i+1]=='0') {
+                    min_ops+=2;
+                }
                 else {
-                    min_ops += 2;
-                    s1[i] = '0';
+
+                    if(i+1==n) {
+                        if(i-1>=0) {
+                            if(s1[i-1] == '1') {
+                                min_ops+=2;
+                            }
+                            else {
+                                min_ops += 2;
+                            }
+                        }
+                    }
                 }
             }
         }
