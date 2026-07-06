c23f6b552ad83d280cbb35e04e2e1e632ba759e6
Mihirpathakji <mihirgithub123456@gmail.com>
2026-07-06 14:00:51 +0530

    Time: 67 ms (8.74%) | Memory: 33.5 MB (5.30%) - LeetSync

--- 1222-remove-covered-intervals/remove-covered-intervals.cpp
+++ 1222-remove-covered-intervals/remove-covered-intervals.cpp
@@ -2,42 +2,48 @@ class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
 
-       int n = intervals.size();
+        int n = intervals.size();
 
         auto mylambda = [](vector<int>a,vector<int>b) {
             
-           if(a[0] == b[0]) {
-                return a[1] > b[1];
-           }
+            //Handle the Edge case first.
 
-           return a[0] < b[0];
+            if(a[0] == b[0]) {
+                return a[1] > b[1];//If first element of both comes out to be same -> Sort in descending order of the second element.
+            }
+
+           return a[0] < b[0];//Else ascending order of the first element.
 
         };
 
         sort(intervals.begin(),intervals.end(),mylambda); 
 
-        vector<vector<int>>result;
-        result.push_back(intervals[0]);
+        int c = intervals[0][0];//1
+        int d = intervals[0][1];//7 
+
+        int counter = 1;//1
 
         for(int i = 1;i < n;i++) 
         {   
-            int c = result.back()[0];
-            int d = result.back()[1];
 
-            int a = intervals[i][0];
-            int b = intervals[i][1];
+            int a = intervals[i][0];//1 1 2
+            int b = intervals[i][1];//4 2 8 
 
             if(c <= a && b <= d) {
-                continue;
+                continue;//Remove this (a,b) pair so dosent contribute to the ans.
             }
+            else {
 
-            result.push_back(intervals[i]);
+                d = b;
+                c = a;
+                counter++;
+            }
         }
 
-        return result.size();
+        return counter;
 
         //TC : O(nlogn)
-        //SC : O(n)
+        //SC : O(1)
 
 
     }
