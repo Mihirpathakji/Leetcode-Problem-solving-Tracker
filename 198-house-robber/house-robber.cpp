7eea66b94cc7ebd4673aa44143a8bd2c5b8a3143
Mihirpathakji <mihirgithub123456@gmail.com>
2026-07-06 20:36:12 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (89.60%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
 
-
     int rob(vector<int>& nums) {
 
-        //Bottom Up :
+        //Space Optimization :
 
         int n = nums.size();
-        vector<int>dp(n+1,-1);
         
-        //1.Initialize the dp with base cases : 
-
-        dp[0] = nums[0];//Max money that can be obtained by robbing houses from 0th till 0th house.
-
         if(n == 1) {
-            return dp[0];
+            return nums[0];
         }
 
-        dp[1] = max(nums[0],nums[1]);//Max money that can be obtained by robbing house from 0th till 1th.
+        int first = nums[0];
+        int second = max(nums[0],nums[1]);
 
-        //dp[i] = Max money that can be obtained by robbing houses from 0th till ith house.
+        int third = second;
 
         for(int i = 2;i < n;i++) {
-            dp[i] = max(nums[i] + dp[i-2] , dp[i-1]); 
+            third = max(nums[i] + first , second);
+            first = second;
+            second = third; 
         }
 
-        return dp[n-1];//Max money obtained by robbing from 0th till n-1 houses.
+        return third;//Max money obtained by robbing from 0th till n-1 houses.
 
         //TC : O(n)
-        //SC : O(n)
+        //SC : O(1)
 
     }
 };
