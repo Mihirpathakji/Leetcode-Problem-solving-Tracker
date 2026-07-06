d3a4f639a62176cac737b9eb0c39606fe49be63a
Mihirpathakji <mihirgithub123456@gmail.com>
2026-07-06 23:12:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (51.63%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -15,7 +15,7 @@ public:
             return max(nums[0],nums[1]);
         }
 
-        vector<int>dp(n-1,-1);//0 to (n-2) th. -> n-2+1 == n-1 elements are there.
+        vector<int>dp(n,-1);//0 to (n-2) th. -> n-2+1 == n-1 elements are there.
 
         dp[0] = nums[0];
      
@@ -25,19 +25,29 @@ public:
             dp[i] = max(nums[i] + dp[i-2],dp[i-1]);
         }
 
-        int maxi1 = dp[n-2];//dp[2]. 
+        int maxi1 = dp[n-2];
 
-        vector<int>dp2(n,-1);//1 to n-1 
+        //1 to n-1 th.
 
-        dp2[0] = nums[1];
-
-        dp2[1] = max(nums[1],nums[2]);   
-        
-        for(int i = 2;i < n-1;i++) {
-            dp2[i] = max(nums[i+1] + dp2[i-2],dp2[i-1]); 
+        vector<int>temp(n,0);
+        for(int i = 1;i < n;i++) {
+            temp[i] = nums[i]; 
         }
 
-        return max(dp2[n-2],maxi1);
+        dp[0] = temp[0];
+        dp[1] = max(temp[0],temp[1]);
+
+        for(int i = 2;i < n;i++) {
+            dp[i] = max(nums[i] + dp[i-2],dp[i-1]);
+        }
+
+        maxi1 = max(maxi1,dp[n-1]);
+
+        return maxi1;
+
+        //TC : O(n)
+        //SC : O(n)
+
 
     }
 };
