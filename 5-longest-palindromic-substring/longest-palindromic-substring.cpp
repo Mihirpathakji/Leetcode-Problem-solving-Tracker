8702ff22aa157136fde068a89e3473506ce42496
Mihirpathakji <mihirgithub123456@gmail.com>
2026-07-09 21:33:37 +0530

    Time: 600 ms (10.83%) | Memory: 292 MB (7.03%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -8,24 +8,27 @@ public:
         if(i >= j) {
             dp[i][j] = 1;
             return true;
-        }
+        }//O(1)
 
-        //Memoization : 
+        //Memoization :- 
 
         if(dp[i][j] !=-1) {
             return true;
-        }
+        }//O(1)
         
         if(s[i] == s[j]) {
+
             //Check if same at every points inside the substring.
 
             if(isPalindrome(dp,s,i+1,j-1)) {
                 dp[i][j] = 1;
                 return true;
-            }
+            }//O(1).
+            
             else {
                 return false;
             }
+            
         }
 
         return false;
@@ -40,6 +43,7 @@ public:
         vector<vector<int>>dp(n,vector<int>(n,-1));//stores that [i,j] substring is a palidnrome or not.
 
         for(int i = 0;i < n;i++) {
+
             for(int j = i;j < n;j++) {
 
                 //Check whether this substring is Palindrome or not.
@@ -50,17 +54,30 @@ public:
                    
                     if(max_len < j-i+1) {
 
-                        max_len = j-i+1;//1 "b" 3 //1
-                        sp = i;//0 0
+                        max_len = j-i+1;//1 "b" 3 //1 3 7
+                        sp = i;//0 0 0 0
                     }
                 }
             }
+
         }
         
 
         return s.substr(sp,max_len);
 
         //TC : O(n^2)
+
+        /*  
+            Total Time Complexity of the Dp soln is :
+
+            Total_work_done_over_all_NON_RECURSIVE_STEPS * Total_Dp_States.
+
+            == O(1)*(O(n^2)).Since dp can have states for all possible n^2 [l,r] substrings.
+        
+        */
+
+
+
         //SC : O(n*2)
         
     }
