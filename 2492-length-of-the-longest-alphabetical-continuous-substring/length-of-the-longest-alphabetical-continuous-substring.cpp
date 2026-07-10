f88cddc2e2f489961fe95d91638a0f4851bd2e7c
Mihirpathakji <mihirgithub123456@gmail.com>
2026-07-11 00:31:19 +0530

    Time: 2 ms (68.82%) | Memory: 17.9 MB (99.03%) - LeetSync

--- 2492-length-of-the-longest-alphabetical-continuous-substring/length-of-the-longest-alphabetical-continuous-substring.cpp
+++ 2492-length-of-the-longest-alphabetical-continuous-substring/length-of-the-longest-alphabetical-continuous-substring.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
+   
     int longestContinuousSubstring(string s) {
-
-        //Sliding window :
-
+        
         int n = s.length();
-        int i = 0;
-        int j = 1;
+        int j = 0;
+        int current_counter = 1;
+
         int max_len = 0;
-        int counter = 1;
+        
+        while(j < n-1) {
 
-        while(j < n) {
-
-            //Consecutives are founded.    
-            if((s[j] -'0') == (s[j-1] - '0' + 1)) {
-                counter++;//2
+            if(s[j+1] == s[j] + 1) {
+                current_counter++;
             }
             else {
-                max_len = max(max_len,counter);//2
-                counter = 1; 
+                max_len = max(max_len,current_counter);
+                current_counter = 1;
             }
             j++;
-
         }
+
+        max_len = max(max_len,current_counter);
         
-        max_len = max(max_len,counter);
         return max_len;
-        
-        
+
     }
 };
