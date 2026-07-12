311877f972253ecfb1e2f9c26ff8e15f6929393a
Mihirpathakji <mihirgithub123456@gmail.com>
2026-07-12 19:55:05 +0530

    Time: 7 ms (37.97%) | Memory: 111.9 MB (78.12%) - LeetSync

--- 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
@@ -8,8 +8,8 @@ public:
         int n = arr.size();
 
         int prefix_sum = 0;
-        int odd_subarr = 0;
-        int even_subarr = 0;
+        int oddsum_count = 0;
+        int evensum_count = 0;
         int ans = 0;
 
         for(int i = 0;i < n;i++) {
@@ -17,17 +17,17 @@ public:
             prefix_sum += arr[i];
 
             if(prefix_sum % 2 == 1) {
-                ans = (ans + even_subarr);
-                odd_subarr++;
+                ans = (ans + evensum_count);
+                oddsum_count++;
                 ans++;
             }
 
             if(prefix_sum % 2 == 0) {
                 //evensumed,if there inside exists a odd sum.Than odd+even = even.
-                ans = (ans + odd_subarr) % MOD;
-                even_subarr++;
+                ans = (ans + oddsum_count) % MOD;
+                evensum_count++;
             }
-   
+
         }
         
         ans %= MOD;
