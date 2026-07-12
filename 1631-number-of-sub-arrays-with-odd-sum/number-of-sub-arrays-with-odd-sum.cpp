e3cd59b954efe1c3f820c03b8d2d84fbc8494745
Mihirpathakji <mihirgithub123456@gmail.com>
2026-07-12 20:02:24 +0530

    Time: 4 ms (56.63%) | Memory: 111.8 MB (95.50%) - LeetSync

--- 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
@@ -7,22 +7,22 @@ public:
 
         int n = arr.size();
 
-        int prefix_sum = 0;
-        int oddsum_count = 0;
-        int evensum_count = 0;
+        int prefix_sum = 0;//0
+        int oddsum_count = 0;//0
+        int evensum_count = 0;//0We will Manually Handle if any prefixsum becomes Odd,we will do add extra one subarr.
         int ans = 0;
 
         for(int i = 0;i < n;i++) {
             
-            prefix_sum += arr[i];
+            prefix_sum += arr[i];//1 
 
             if(prefix_sum % 2 == 1) {
                 ans = (ans + evensum_count);
                 oddsum_count++;
-                ans++;
+                ans++;//1 
             }
 
-            if(prefix_sum % 2 == 0) {
+            else {
                 //evensumed,if there inside exists a odd sum.Than odd+even = even.
                 ans = (ans + oddsum_count) % MOD;
                 evensum_count++;
