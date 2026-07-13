455ab124da646140050bd227a2bcae9eed2b6b25
Mihirpathakji <mihirgithub123456@gmail.com>
2026-07-13 21:38:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (76.91%) - LeetSync

--- 1212-sequential-digits/sequential-digits.cpp
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -4,23 +4,25 @@ public:
     vector<int> sequentialDigits(int low, int high)  {
 
         string universal = "123456789";
-        string l1 = to_string(low);
-        string l2 = to_string(high);
+        string l1 = to_string(low);//TC : O(1) here.//3.
+        string l2 = to_string(high);//3 
 
         vector<int>result;
 
-        int size_l1 = l1.length();
-        int size_l2 = l2.length(); 
+        int size_l1 = l1.length();//3
+        int size_l2 = l2.length();//3 
 
         for(int i = size_l1;i <= size_l2;i++) {
 
             for(int j = 0;j <= 9-i;j++) {
 
                 string ans = universal.substr(j,i);
-                int number = stoi(ans);
+                int number = stoi(ans);//345
+
+                //The number must lies in between the boundaries inorder to be in interval.
 
                 if(number >= low && number <= high) {
-                    result.push_back(number);
+                    result.push_back(number);//123 234 
                 }
             }
         }
