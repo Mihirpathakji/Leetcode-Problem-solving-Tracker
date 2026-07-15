c8cfdebd2f8da2e0f90d48ba4ba789416e680fbd
Mihirpathakji <mihirgithub123456@gmail.com>
2026-07-15 10:29:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (0.36%) - LeetSync

---
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+
+        long double odd_sum = 0;//O(1)
+        
+        long double mul1 = (double)n/2;
+
+        long long mul2 = (2*1 + (n-1)*2);
+
+        odd_sum = (mul1*mul2);
+
+
+        
+        long double even_sum = 0;//O(1)
+        
+        mul1 = (double)n/2;
+
+        mul2 = (2*2 + (n-1)*2);
+
+        even_sum = (mul1*mul2);
+
+        int sum1 = odd_sum;
+        int sum2 = even_sum;
+
+        
+        return __gcd(sum1,sum2);
+
+    }
+};
