2e9ec7a243c359cff0c0e0de044ce045061adec9
Mihirpathakji <mihirgithub123456@gmail.com>
2026-07-20 21:19:49 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (66.35%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.cpp
+++ 1386-shift-2d-grid/shift-2d-grid.cpp
@@ -1,36 +1,44 @@
 class Solution {
 public:
-    
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
 
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<int>>temp(m,vector<int>(n));
+        vector<vector<int>>ans(m,vector<int>(n));
+
+        //shifting 2d matrix is same as shifting 1d array.
+
+        int total_cols = n;
         int total_index = m*n;
 
         for(int i = 0;i < m;i++) {
             for(int j = 0;j < n;j++) {
 
-                int curr_row = i;//0 0 0 1 1 1
-                int curr_col = j;//0 1 2 0 1 2 
- 
-                int index_in_1d = (curr_row * n) + curr_col;//0 1 2 3 4 5
+                int curr_row = i;
+                int curr_col = j;
 
-                int new_index_in_1d = (index_in_1d + k )% total_index;//1 2 4 5 6 
+                int index_in_1d = (curr_row * total_cols) + curr_col;
 
-                int new_row = (new_index_in_1d / n);//0 0 1 1 2
-                int new_col = new_index_in_1d % n;//1 2 1 2 2
+                //shifted the 1d array by k places.
 
-                temp[new_row][new_col] = grid[curr_row][curr_col];// 
+                int new_index_in_1d = ( index_in_1d + k ) % total_index;      
+
+                //Converting 1d to New 2d matrix again :        
+
+                int new_row = new_index_in_1d / total_cols;
+                int new_col = new_index_in_1d % total_cols;
+
+                ans[new_row][new_col] = grid[curr_row][curr_col];
 
             }
         }
 
-        return temp;//  
-
-        //TC : O(m*n) 
+        return ans;
+        
+        //TC : O(m*n)
         //SC : O(m*n)
+
         
     }
 };
