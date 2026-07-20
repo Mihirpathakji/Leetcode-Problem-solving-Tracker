c3a1e8796f668d324412098d2dda9404ca9006ca
Mihirpathakji <mihirgithub123456@gmail.com>
2026-07-20 20:48:40 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (66.35%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.cpp
+++ 1386-shift-2d-grid/shift-2d-grid.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
+    
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
 
         int m = grid.size();
         int n = grid[0].size();
 
         vector<vector<int>>temp(m,vector<int>(n));
+        int total_index = (m-1)*n + n;
 
-        temp = grid; 
+        for(int i = 0;i < m;i++) {
+            for(int j = 0;j < n;j++) {
 
-        for(int i = 0;i < k; i++) {
+                int curr_row = i;//0 0 0 1 1 1
+                int curr_col = j;//0 1 2 0 1 2
+ 
+                int index_in_1d = (curr_row * n) + curr_col;//0 1 2 3 4 5
 
-            //1.i = 0.
-            
-            for(int l = 0;l < m; l++) {
-                for(int j = 0;j < n-1;j++) {
-                    temp[l][j+1] = grid[l][j];//temp[0][0] = grid[0][1]  .This grid should not be the original one it should be the one that accumulates the changed grid values.
-                }
+                int new_index_in_1d = (index_in_1d + k) % total_index;//1 2 4 5 6
+
+                int new_row = (new_index_in_1d / n);//0 0 1 1
+                int new_col = new_index_in_1d % n;//1 2 1 2 
+
+                temp[new_row][new_col] = grid[curr_row][curr_col];// 
+
             }
-
-            for(int l = 0;l < m-1; l++) {
-                for(int j = n-1;j < n;j++) {
-                    temp[l+1][0] = grid[l][j];
-                }
-            } 
-
-            temp[0][0] = grid[m-1][n-1];// 9.    
-            
-            grid = temp;//changed the grid to accumulate the changes done it.
-
         }
 
-        return grid;
+        return temp;//  
+
+        //TC : O(m*n) 
+        //SC : O(m*n)
         
     }
 };
