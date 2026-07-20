84a99aa5de9d44461fa7f2c9a1f87111372df26d
Mihirpathakji <mihirgithub123456@gmail.com>
2026-07-20 20:50:38 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (66.35%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.cpp
+++ 1386-shift-2d-grid/shift-2d-grid.cpp
@@ -7,20 +7,20 @@ public:
         int n = grid[0].size();
 
         vector<vector<int>>temp(m,vector<int>(n));
-        int total_index = (m-1)*n + n;
+        int total_index = m*n;
 
         for(int i = 0;i < m;i++) {
             for(int j = 0;j < n;j++) {
 
                 int curr_row = i;//0 0 0 1 1 1
-                int curr_col = j;//0 1 2 0 1 2
+                int curr_col = j;//0 1 2 0 1 2 
  
                 int index_in_1d = (curr_row * n) + curr_col;//0 1 2 3 4 5
 
-                int new_index_in_1d = (index_in_1d + k) % total_index;//1 2 4 5 6
+                int new_index_in_1d = (index_in_1d + k )% total_index;//1 2 4 5 6 
 
-                int new_row = (new_index_in_1d / n);//0 0 1 1
-                int new_col = new_index_in_1d % n;//1 2 1 2 
+                int new_row = (new_index_in_1d / n);//0 0 1 1 2
+                int new_col = new_index_in_1d % n;//1 2 1 2 2
 
                 temp[new_row][new_col] = grid[curr_row][curr_col];// 
 
