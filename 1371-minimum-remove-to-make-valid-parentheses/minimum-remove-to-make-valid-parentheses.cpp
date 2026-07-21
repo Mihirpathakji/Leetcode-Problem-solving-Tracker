c392f811857ea3fdbe1512dd3109282cc104a085
Mihirpathakji <mihirgithub123456@gmail.com>
2026-07-22 00:35:45 +0530

    Time: 7 ms (68.59%) | Memory: 14.2 MB (68.24%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -4,12 +4,23 @@ public:
        
         //Space Optimized : 
 
+        /*  
+                 Intuition : 
+
+            ->Either any left bracket cannot be balanced or right bracket cannot be balanced.
+            
+            ->Left bracket cannot be balanced only when there does not exists a valid right ")" over the entire string from it's index , so it's index would remain in the stack.
+
+            -> If any right bracket cannot be balanced -> There does not exists any left bracket to balanced it.So stack would be empty for that right bracket.
+
+        */
+
         int n = s.length();
-        stack<int>left_index_to_be_removed;
+        stack<int>left_index_to_be_removed;//
 
         for(int i = 0;i < n;i++) {
-            if(s[i] == '(') {
-                left_index_to_be_removed.push(i);
+            if(s[i] == '(') { 
+                left_index_to_be_removed.push(i); // [2 3]
             }
             else if (s[i] == ')') {
                 if(!left_index_to_be_removed.empty()) {
@@ -19,7 +30,7 @@ public:
                     left_index_to_be_removed.pop();
                 }
                 else {
-                    s[i] = 'I';
+                    s[i] = 'I';//This ')' cannot be balanced , we need to remove it.Marked it as invalid.
                 }
             }
         } 
@@ -28,8 +39,9 @@ public:
             //for this left brackets there does not exists any right inorder to balanced out them.
 
             //they will remain unbalanced.
-            s[left_index_to_be_removed.top()] = 'I';
+            s[left_index_to_be_removed.top()] = 'I';//An invalid char which cannot be there in the s.
             left_index_to_be_removed.pop();
+
         }
 
         string ans;
@@ -41,7 +53,7 @@ public:
         }
 
         return ans;
-        
+
         //TC : O(n)
         //SC : O(1)
 
