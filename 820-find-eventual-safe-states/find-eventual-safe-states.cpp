8a20d2e56e324bf23d121e5de6bd8f16ddd28062
Mihirpathakji <mihirgithub123456@gmail.com>
2026-07-22 17:29:32 +0530

    Time: 53 ms (32.04%) | Memory: 66.2 MB (35.82%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,63 +1,50 @@
-//JAY SHREE RAM.
 class Solution {
 public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
 
-    bool dfs(int start,vector<int>&visited,vector<int>&Same_Path_visited,vector<vector<int>>&graph,vector<int>&safe_node) {
+        int V = graph.size();
+        vector<vector<int>>adj_rev(V);
+        vector<int>indegree(V,0);
 
-        visited[start] = 1;
-        Same_Path_visited[start] = 1;
-
-        for(int &v : graph[start] ) {
-
-            if(visited[v] == 0) {
-
-                if( dfs(v,visited,Same_Path_visited,graph,safe_node) == true ) {
-                    safe_node[v] = 0;
-                    return true;
-                }
-            }
-            else {
-                if(Same_Path_visited[v] == 1) {
-                    safe_node[v] = 0;
-                    return true;
-                }
+        for(int i = 0;i < V;i++) {
+            for(auto it : graph[i]) {
+                adj_rev[it].push_back(i);
+                indegree[i]++;
             }
         }
 
-        safe_node[start] = 1;
-
-        Same_Path_visited[start] = 0;
-
-        return false;
-
-    }
-
-
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-
-        int n = graph.size();
-        vector<int>visited(n,0);
-        vector<int>Same_Path_visited(n,0);  
-        vector<int>safe_node(n,0);
-
-        for(int i = 0;i < n;i++) {
-            if(visited[i] == 0) {
-                dfs(i,visited,Same_Path_visited,graph,safe_node);
+        queue<int>q;
+        for(int i = 0;i < V;i++) {
+            //reversed indegree==0 ->terminal
+            //normal outdegree == 0
+            if(indegree[i] == 0 ) {
+                q.push(i);
             }
-        }    
+        }
 
         vector<int>ans;
 
-        for(int i = 0;i < n;i++) {
-            if(safe_node[i] == 1) {
-                ans.push_back(i);
+        while(!q.empty()) {
+
+            int node = q.front();
+            q.pop();
+
+            ans.push_back(node);
+
+            for(auto& adj_nodes : adj_rev[node] ) {
+                indegree[adj_nodes]--;
+                if(indegree[adj_nodes] == 0) {
+                    q.push(adj_nodes);
+                }
             }
         }
 
+        sort(ans.begin(),ans.end());
         return ans;
 
         //TC : O(V + E)
         //SC : O(V)
+
         
     }
 };
