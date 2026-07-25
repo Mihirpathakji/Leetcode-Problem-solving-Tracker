92077b079d6cd01bf26cc0a3be7dfb5b36af1ed1
Mihirpathakji <mihirgithub123456@gmail.com>
2026-07-25 18:55:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (74.03%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
@@ -4,16 +4,25 @@ public:
 
         //Our goal is to get that problem Accepted anyhow you can use as many spaces as you wanted.Like if you used Maximum space complexity USE IT NOW.Our goal is only to get that problem Accepted.
 
-        vector<int>temp;
+        int maxi1 = 0;
+        int maxi2 = 0;
+
         while(n) {
 
-            int r = n % 10;
-            n/= 10;
+            int r = n % 10; //9 5
+            n/= 10;//95 9 
 
-            temp.push_back(r);
+            if(r >= maxi1) {
+                maxi2 = maxi1;//0 6
+                maxi1 = r;//6 9
+            }
+            else if(r > maxi2) {
+                maxi2 = r;
+            }
+
         }
 
-        sort(temp.rbegin(),temp.rend());
-        return temp[0]*temp[1];
+        return maxi1*maxi2;
+
     }
 };
