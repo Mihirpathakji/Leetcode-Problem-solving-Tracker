69a9178752ebf9cbd37a853de263d5e29a81e6b2
Mihirpathakji <mihirgithub123456@gmail.com>
2026-07-29 12:50:51 +0530

    Time: 169 ms (11.28%) | Memory: 75.2 MB (37.09%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -3,47 +3,44 @@ public:
     string smallestPalindrome(string s) {
        
         int n = s.length();
-        if(n == 1) {
-            return s;
-        }
 
         vector<int>counts(26,0);
-        vector<int>taken(26,-1);
         vector<int>needed(26,-1);
 
         for(int i = 0;i < n;i++) {
-            counts[s[i]-'a']++;//
+            counts[s[i]-'a']++;
         }
 
         string ans = "";
-        set<char>st(s.begin(),s.end());//[ab]//[z].//[rur]
+        set<char>st(s.begin(),s.end());
 
         for(auto it : st) {
-            int temp_count = counts[it-'a']/2;//1.1.0.//1.0.
-            if(temp_count) {
-                taken[it-'a'] = 1;
-            }
+
+            int temp_count = counts[it-'a']/2;
+
             while(temp_count--) {
-                ans.push_back(it);//ab // r
+                ans.push_back(it);
             }
-            if(counts[it-'a']%2 == 1) {
+
+            if(counts[it-'a'] % 2 == 1) {
                 needed[it-'a'] = 1;
             }
+
         }
 
-        string temp = ans;//ab "" //r.//r.
+        string temp = ans;
 
         for(auto it : st) {
-            if(taken[it-'a'] == -1 || (needed[it-'a'] ==1)) {
+            if(needed[it-'a'] == 1) {
                 ans.push_back(it);
-            }//ru
+            }
         }
 
-        reverse(temp.begin(),temp.end());//ba //r.
+        reverse(temp.begin(),temp.end());
 
-        ans += temp;//abbba //
+        ans += temp;
+
         return ans;
-
 
     }
 };
