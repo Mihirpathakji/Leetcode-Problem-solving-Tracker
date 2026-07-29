108980475725b7b00647c1c59ef6595726eb7767
Mihirpathakji <mihirgithub123456@gmail.com>
2026-07-29 13:09:37 +0530

    Time: 189 ms (7.59%) | Memory: 75.2 MB (37.09%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -12,38 +12,40 @@ public:
         }
 
         string ans = "";
+      
         set<char>st(s.begin(),s.end());//Sorted order we needed.And no duplicates.
+        //O(n*log(26)) // n*log(SET SIZE).
 
         for(auto it : st) {
 
-            int temp_count = counts[it-'a']/2;
+            int temp_count = counts[it-'a']/2;//1 1 1 
 
             while(temp_count--) {
-                ans.push_back(it);//ab
+                ans.push_back(it);//ab //[a c d]. 
             }
 
             if(counts[it-'a'] % 2 == 1) {
                 needed[it-'a'] = 1;
             }
 
-        }
+        }//O(1).
 
-        string temp = ans;
+        string temp = ans;//abd
 
         for(auto it : st) {
             if(needed[it-'a'] == 1) {
                 ans.push_back(it);
             }
-        }
+        }//O(1).
 
-        reverse(temp.begin(),temp.end());//ba
+        reverse(temp.begin(),temp.end());//dca
 
         ans += temp;
 
         return ans;
         
-        //TC : O(n*logn)
-        //SC : O(n)
+        //TC : O(n*log(Maximum set size)) == O(n*log(26)).
+        //SC : O(n).//->ans.
 
     }
 };
