649897b2e2f1f77a3f00e1605264d672c035f8a3
Mihir Pathakji <mihirgithub123456@gmail.com>
2026-08-14 20:29:43 +0530

    Time: 4 ms (8.64%) | Memory: 50 MB (5.87%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
@@ -5,7 +5,7 @@ typedef long long ll;
 
     int maxScore(vector<int>& cardPoints, int k) {
 
-    int n = cardPoints.size();
+    int n = cardPoints.size();//
 
     ll max_sum = 0;
 
@@ -22,8 +22,7 @@ typedef long long ll;
     }
 
     vector<int>pf_right(n,0);
-    reverse(cardPoints.begin(),cardPoints.end());//
-    //[1,6,5,4,3,2,1].
+    reverse(cardPoints.begin(),cardPoints.end());
 
     pf_right[0] = cardPoints[0];//
 
@@ -31,30 +30,38 @@ typedef long long ll;
         pf_right[i] = pf_right[i-1] + cardPoints[i];
     }
 
-    int i = k-1;//2.
-    //combinations : [k,0]
-    //
+    //[2,4,6]
+    //[2,4,6]
+
+    int i = k-1;//1.
 
     while(i >= 0) {
 
-        ll left_sum = 0;//0.//since this is a new combination of the left sum and the right sum.//0.
-        ll right_sum = 0;//0//0.
+        ll left_sum = 0;//0.
+     
+        ll right_sum = 0;//0.
         
-        left_sum = pf_left[i];//6.//3 elements selected.//6 3 1.
+        left_sum = pf_left[i];//55.46.39.32.23.16.9. // 4.2.
+        
+        //  [k,0].
 
-        int number_of_ele_right = k - (i+1);//0.1.2.
+        ll number_of_ele_right = k - (i+1);//0.1.2.5.6.//0.1.
 
-        if( number_of_ele_right - 1 >= 0)
-        right_sum = pf_right[number_of_ele_right - 1];//0.1.7.
+        if( number_of_ele_right - 1 >= 0) {
+            right_sum = pf_right[number_of_ele_right - 1];//9.16.23 32.55.39.46.//2.
+        }
 
-        max_sum = max(left_sum + right_sum,max_sum);//6.8.
+        max_sum = max( left_sum + right_sum ,max_sum);//55.55.55.55.55.55.55.
+        //4.4.
 
-        i--;//1.0.-1.
+        i--;//5.4.3.2.1.0.-1.0.-1.
     }
 
-    max_sum = max(max_sum,(ll)pf_right[k-1]);
+    max_sum = max(max_sum,(ll)pf_right[k-1]);//55.4.
 
-    return max_sum;
+    return max_sum;//
+    //TC : O(N)
+    //SC : O(N)
  
 }
 
