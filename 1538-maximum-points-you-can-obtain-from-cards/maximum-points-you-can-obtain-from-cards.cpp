73122b852fa565d56608474d830720cc2486d91a
Mihir Pathakji <mihirgithub123456@gmail.com>
2026-08-14 23:20:43 +0530

    Time: 0 ms (100.00%) | Memory: 49.9 MB (5.87%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
@@ -9,10 +9,6 @@ typedef long long ll;
 
     ll max_sum = 0;
 
-    if(n == 1) {
-        return cardPoints[0];
-    }
-
     vector<int>pf_left(n,0);
     
     pf_left[0] = cardPoints[0];
@@ -30,36 +26,31 @@ typedef long long ll;
         pf_right[i] = pf_right[i-1] + cardPoints[i];
     }
 
-    //[2,4,6]
-    //[2,4,6]
-
-    int i = k-1;//1.
+    int i = k-1;//
 
     while(i >= 0) {
 
-        ll left_sum = 0;//0.
+        ll left_sum = 0;//
      
-        ll right_sum = 0;//0.
+        ll right_sum = 0;//
         
-        left_sum = pf_left[i];//55.46.39.32.23.16.9. // 4.2.
-        
-        //  [k,0].
+        left_sum = pf_left[i];//1.
 
-        ll number_of_ele_right = k - (i+1);//0.1.2.5.6.//0.1.
+        ll number_of_ele_right = k - (i+1);//0.
 
         if( number_of_ele_right - 1 >= 0) {
-            right_sum = pf_right[number_of_ele_right - 1];//9.16.23 32.55.39.46.//2.
+            right_sum = pf_right[number_of_ele_right - 1];
         }
 
-        max_sum = max( left_sum + right_sum ,max_sum);//55.55.55.55.55.55.55.
-        //4.4.
+        max_sum = max( left_sum + right_sum ,max_sum);//
 
-        i--;//5.4.3.2.1.0.-1.0.-1.
+        i--;//
     }
 
-    max_sum = max(max_sum,(ll)pf_right[k-1]);//55.4.
+    max_sum = max(max_sum,(ll)pf_right[k-1]);//
 
-    return max_sum;//
+    return max_sum;
+
     //TC : O(N)
     //SC : O(N)
  
