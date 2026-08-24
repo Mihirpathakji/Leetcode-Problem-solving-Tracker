95334f82ef8b6055be93a98e92f29dd29a1af7c4
Mihir Pathakji <mihirgithub123456@gmail.com>
2026-08-24 21:38:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (99.36%) - LeetSync

---
+++ 4264-first-matching-character-from-both-ends/first-matching-character-from-both-ends.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int firstMatchingIndex(string s) {
+
+        int n = s.length(); 
+
+        int i = 0;
+
+        while(i <= n/2) {
+
+            if(s[i]==s[n-i-1]) {
+                return i;
+            }
+            i++;
+        }
+
+        return -1;
+
+    }
+};
