110cbf9f976045ebff265c10253c1a4760fba105
Mihir Pathakji <mihirgithub123456@gmail.com>
2026-09-13 18:12:42 +0530

    Time: 0 ms (100.00%) | Memory: 97.3 MB (88.10%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -5,32 +5,29 @@ public:
         
         int n = prices.size();
 
-        int i = 0;
-        int j = n-1;
-
-        int max_profit = 0;//0.//0.
+        int max_profit = 0;//0.
         
-        int right_max = prices[n-1];//1.//4.//1.
+        int right_max = prices[n-1];//4.
 
-        int best_i = n-2;//4.//3.
-        int best_j = n-1;//5..//4.
+        int best_i = n-2;//
+        int best_j = n-1;//
 
-        int right_max_index = n-1;//At every iteration the right_max_index == index of the right_max element.
+        int right_max_index = n-1;//At every iteration the right_max_index == index of the right_max element.//5.
 
         for(int i = n-2;i>=0;i-- ) {
 
+            //this {i,right_max_index} is giving the maximum profit.
+
             if(max_profit < right_max-prices[i]) {
-                best_i = i;
+                best_i = i;//
                 best_j = right_max_index;//Index of the right_max element.
-                max_profit = right_max - prices[i];
+                //
+                max_profit = right_max - prices[i];//
             }
 
-            else {
-
-                if(right_max < prices[i]) {
-                    right_max = prices[i];
-                    right_max_index = i;
-                }
+            if(prices[i] > right_max) {
+                right_max = prices[i];
+                right_max_index = i;
             }
 
         }
