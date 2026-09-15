9fec9069bc7e01446bdced66dc2019457039e0cd
Mihir Pathakji <mihirgithub123456@gmail.com>
2026-09-15 19:41:13 +0530

    Time: 3 ms (9.71%) | Memory: 9.5 MB (30.40%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
 
+    int solve(int m,int n,int i,int j,vector<vector<int>>&dp) {
+        
+        
+        if(i < 0 || j < 0) {
+            return 0;
+        }
+
+        if(dp[i][j]!=-1) {
+            return dp[i][j];
+        }
+ 
+        if(i == j && j == 0) {
+            return dp[i][j] = 1;
+        }
+
+        if( (i == 0) || ( j == 0 )) {
+            return dp[i][j] = 1;
+        }
+
+        return dp[i][j] = solve(m,n,i-1,j,dp) + solve(m,n,i,j-1,dp);//
+
+    }
+
     int uniquePaths(int m, int n) {
 
         //unique paths to reach arr[m-1][n-1] = unique paths to reach arr[m-1-1][n-1] + unique paths to reach arr[m-n][n-1-1].
@@ -12,23 +35,15 @@ public:
         //Base case are i = 0 && j = 1 only 1 step to reach it.
         //Base case are i = 1 && j = 0 only 1 step to reach it.
 
+        int i = m-1;
+        int j = n-1;
+
         vector<vector<int>>dp(m,vector<int>(n,-1));
 
-        for(int j = 0;j < n;j++) {
-            dp[0][j] = 1;
-        }
+        return solve(m,n,i,j,dp);
 
-        for(int i = 0;i < m;i++) {
-            dp[i][0] = 1;
-        }
-
-        for(int i = 1;i < m;i++) {
-            for(int j = 1;j < n;j++) {
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
-            }
-        }
-
-        return dp[m-1][n-1];
+        //TC : 
+        //SC : 
 
     }
 };
