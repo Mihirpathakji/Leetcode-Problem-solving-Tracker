c1419d06fe46b075c55572a743c1718d384b0022
Mihir Pathakji <mihirgithub123456@gmail.com>
2026-09-17 18:04:24 +0530

    Time: 31 ms (40.27%) | Memory: 27.3 MB (72.16%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,39 +1,63 @@
 class Solution {
 public:
 
-    int solve(vector<vector<int>>&dp,int index1,int index2,string& text1,string& text2,int& n,int& m) {
-
-        if(index1 >= n || index2 >= m) {
-            return 0;
-        }
-
-        if(dp[index1][index2]!=-1) {
-            return dp[index1][index2];
-        }
-
-        //match :
-
-        if(text1[index1] == text2[index2]) {
-            return dp[index1][index2] = 1 + solve(dp,index1+1,index2+1,text1,text2,n,m);
-        }
-
-        // not match : 
-
-        return dp[index1][index2] = max(solve(dp,index1+1,index2,text1,text2,n,m),solve(dp,index1,index2+1,text1,text2,n,m));
-
-    }
-
     int longestCommonSubsequence(string text1, string text2) {
-
-        int index1 = 0;
-        int index2 = 0;
 
         int n = text1.length();
         int m = text2.length();
 
-        vector<vector<int>>dp(n,vector<int>(m,-1));
+        vector<vector<int>>dp(n,vector<int>(m,0));//
 
-        return solve(dp,index1,index2,text1,text2,n,m);
-        
+        int index1 = -1;    
+
+        for(int i = m-1;i>=0;i--) {
+            if(text1[n-1] == text2[i]) {
+                index1 = i;
+                break;
+            }
+        }    
+
+        if(index1>=0) {
+            for(int i = index1;i>=0;i--) {
+                dp[n-1][i] = 1;
+            }
+        }
+
+        int index2 = -1;
+        for(int j = n-1;j>=0;j--) {
+            if(text1[j] == text2[m-1]) {
+                index2 = j;
+                break;
+            }
+        }    
+
+        if(index2>=0) {
+            for(int i = index2;i>=0;i--) {
+                dp[i][m-1] = 1;//.
+            }   
+        }
+
+        for(int i = n-2;i>=0;i--) {
+            for(int j = m-2;j>=0;j--) {
+
+                int match = -1;
+                
+                int not_match = -1;
+
+                if(text1[i] == text2[j]) {
+                    match  = 1 + dp[i+1][j+1];
+                }
+                else {
+                    not_match = max(dp[i+1][j],dp[i][j+1]);//
+                }
+                
+                dp[i][j] = max(match,not_match);
+            
+            }
+        }
+
+        return dp[0][0];
+
+
     }
 };
